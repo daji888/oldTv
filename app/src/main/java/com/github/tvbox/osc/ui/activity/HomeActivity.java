30819fb19493d395aea7d3492836f53f758570b6
daji888 <97334834+daji888@users.noreply.github.com>
2024-01-11 22:33:08 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -94,7 +94,7 @@ public class HomeActivity extends BaseActivity {
         public void run() {
             Date date = new Date();
             @SuppressLint("SimpleDateFormat")
-            SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd EE HH:mm");
             tvDate.setText(timeFormat.format(date));
             mHandler.postDelayed(this, 1000);
         }
