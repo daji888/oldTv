e0f713e6982e6cb0b4dd61b5249572ca2e5cccae
daji888 <97334834+daji888@users.noreply.github.com>
2024-02-10 22:47:05 +0800

    更新 ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -65,10 +65,10 @@ public class ApiDialog extends BaseDialog {
                     ArrayList<String> history = Hawk.get(HawkConfig.API_HISTORY, new ArrayList<String>());
                     if (!history.contains(newApi))
                         history.add(0, newApi);
-                        history.add(1, "http://www.饭太硬.top/tv");
-                        history.add(2, "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json");
-                        history.add(3, "http://cdn.qiaoji8.com/tvbox.json");
-                        history.add(4, "https://ghproxy.net/raw.githubusercontent.com/daji888/ys/master/ys99.txt");
+                    history.add(1, "http://www.饭太硬.top/tv");
+                    history.add(2, "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json");
+                    history.add(3, "http://cdn.qiaoji8.com/tvbox.json");
+                    history.add(4, "https://ghproxy.net/raw.githubusercontent.com/daji888/ys/master/ys99.txt");
                     if (history.size() > 30)
                         history.remove(30);
                     Hawk.put(HawkConfig.API_HISTORY, history);
