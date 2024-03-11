15d95ca6ffc6008810fdadfb413c04238bd0edc0
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-11 22:24:55 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -65,7 +65,6 @@ public class EXOmPlayer extends ExoMediaPlayer {
         return data;
     }
 
-
     @SuppressLint("UnsafeOptInUsageError")
     private void getExoSelectedTrack() {
         audioId = "";
