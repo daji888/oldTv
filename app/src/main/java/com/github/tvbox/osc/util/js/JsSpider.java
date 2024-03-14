05c3a5b3afe9e6515d7fc380bac551e5a2a0f6b4
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-14 15:47:55 +0800

    Update JsSpider.java

--- app/src/main/java/com/github/tvbox/osc/util/js/JsSpider.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/JsSpider.java
@@ -31,7 +31,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import java9.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletableFuture;
 
 public class JsSpider extends Spider {
 
