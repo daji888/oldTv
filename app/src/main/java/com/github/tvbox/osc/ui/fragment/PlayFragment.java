6af56a201970b5d76e32a9e011802aefc648a3d0
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-14 22:35:06 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -283,11 +283,6 @@ public class PlayFragment extends BaseLazyFragment {
             }
 
             @Override
-            public void openVideo() {
-                openMyVideo();
-            }
-
-            @Override
             public void prepared() {
                 initSubtitleView();
             }
@@ -543,15 +538,6 @@ public class PlayFragment extends BaseLazyFragment {
             }
         }, bean, trackInfo.getAudioSelected(false));
         dialog.show();
-    }
-
-    void openMyVideo() {
-        Intent i = new Intent();
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setAction(android.content.Intent.ACTION_VIEW);
-        if (videoURL == null) return;
-        i.setDataAndType(Uri.parse(videoURL), "video/*");
-        startActivity(Intent.createChooser(i, "Open Video with ..."));
     }
 
     void setTip(String msg, boolean loading, boolean err) {
