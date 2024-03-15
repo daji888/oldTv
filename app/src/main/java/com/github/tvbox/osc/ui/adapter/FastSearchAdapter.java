201af026ce3f3932643fbef71786368ac7137743
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-15 20:52:20 +0800

    Update FastSearchAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/FastSearchAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/FastSearchAdapter.java
@@ -39,7 +39,7 @@ public class FastSearchAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
                     .load(item.pic)
                     .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                             .centerCorp(true)
-                            .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
+                            .override(AutoSizeUtils.mm2px(mContext, 228), AutoSizeUtils.mm2px(mContext, 296))
                             .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
                     .noFade()
