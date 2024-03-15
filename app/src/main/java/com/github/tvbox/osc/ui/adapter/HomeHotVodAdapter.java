e78232b1ff388f751546b92a10973598e8d6e8ff
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-15 20:42:09 +0800

    Update HomeHotVodAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -63,7 +63,7 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
                     .load(DefaultConfig.checkReplaceProxy(item.pic))
                     .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                             .centerCorp(true)
-                            .override(AutoSizeUtils.mm2px(mContext, 214), AutoSizeUtils.mm2px(mContext, 280))
+                            .override(AutoSizeUtils.mm2px(mContext, 228), AutoSizeUtils.mm2px(mContext, 296))
                             .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
                     .noFade()
