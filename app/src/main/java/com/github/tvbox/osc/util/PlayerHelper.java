9ea6d53960404d6cfc2bbee5b8a997bf1f8be109
daji888 <97334834+daji888@users.noreply.github.com>
2024-03-31 15:42:26 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -37,7 +37,7 @@ public class PlayerHelper {
     public static void updateCfg(VideoView videoView, JSONObject playerCfg, int forcePlayerType) {
         int playerType = Hawk.get(HawkConfig.PLAY_TYPE, 0);
         int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
-        String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "软解码");
+        String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "硬解码");
         int scale = Hawk.get(HawkConfig.PLAY_SCALE, 0);
         try {
             playerType = playerCfg.getInt("pl");
