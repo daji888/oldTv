103f44643b585db15fade467f99e88c4e9dce1f6
daji888 <97334834+daji888@users.noreply.github.com>
2024-04-17 15:51:43 +0800

    Create BindingContext.java

---
+++ quickjs/src/main/java/com/github/tvbox/quickjs/wrapper/BindingContext.java
@@ -0,0 +1,30 @@
+package com.github.tvbox.quickjs;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BindingContext {
+
+    protected Map<String, Method> functionMap;
+
+    protected Method contextSetter;
+
+    public BindingContext() {
+        functionMap = new HashMap<>();
+        contextSetter = null;
+    }
+
+    public Map<String, Method> getFunctionMap() {
+        return functionMap;
+    }
+
+
+    public Method getContextSetter() {
+        return contextSetter;
+    }
+
+    public void setContextSetter(Method contextSetter) {
+        this.contextSetter = contextSetter;
+    }
+}
