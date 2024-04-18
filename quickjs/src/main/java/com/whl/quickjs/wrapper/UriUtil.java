7c908647fb74d186e561ed52205f24c18cf23935
daji888 <97334834+daji888@users.noreply.github.com>
2024-04-18 11:55:07 +0800

    Update UriUtil.java

--- quickjs/src/main/java/com/whl/quickjs/wrapper/UriUtil.java
+++ quickjs/src/main/java/com/whl/quickjs/wrapper/UriUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.tvbox.quickjs;
+package com.whl.quickjs.wrapper;
 
 import android.net.Uri;
 import android.text.TextUtils;
