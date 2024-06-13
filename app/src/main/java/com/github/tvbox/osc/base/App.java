350751e6a53592110f8ea5a744cc1d27703ddd68
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-13 14:06:19 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -75,7 +75,7 @@ public class App extends MultiDexApplication {
         putDefault(HawkConfig.HISTORY_NUM, 2);               //历史条数: 0=50条, 1=100条, 2=200条
         // 播放器选项
         putDefault(HawkConfig.SHOW_PREVIEW, false);          //小屏预览: true=开启, false=关闭
-        putDefault(HawkConfig.PLAY_RENDER, 1);               //渲染方式: 0=Textureview, 1=Surfaceview
+        putDefault(HawkConfig.PLAY_RENDER, 0);               //渲染方式: 0=Textureview, 1=Surfaceview
         putDefault(HawkConfig.PLAY_SCALE, 0);                //画面缩放: 0=默认, 1=16:9, 2=4:3, 3=填充, 4=原始, 5=裁剪
         putDefault(HawkConfig.PLAY_TYPE, 2);                 //播放器: 0=系统, 1=IJK, 2=Exo, 3=MX, 4=Reex, 5=Kodi
         putDefault(HawkConfig.IJK_CODEC, "硬解码");           //IJK解码: 软解码, 硬解码
