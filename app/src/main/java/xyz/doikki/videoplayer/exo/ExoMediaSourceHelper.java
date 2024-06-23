6c687ee22c20dff3567682155609fcdc4d7d389f
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-24 01:04:24 +0800

    Update ExoMediaSourceHelper.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -108,7 +108,7 @@ public final class ExoMediaSourceHelper {
         } else if ("rtsp".equals(contentUri.getScheme())) {
             return new RtspMediaSource.Factory().createMediaSource(MediaItem.fromUri(contentUri));
         }
-        int contentType = Util.inferContentType(uri);
+        int contentType = inferContentType(uri);
         DataSource.Factory factory;
         if (isCache) {
             factory = getCacheDataSourceFactory();
@@ -140,7 +140,19 @@ public final class ExoMediaSourceHelper {
         }
     }
 
-    
+    @SuppressLint("UnsafeOptInUsageError")
+    private int inferContentType(String fileName) {
+        fileName = fileName.toLowerCase();
+        if (fileName.contains(".mpd") || fileName.contains("type=mpd")) {
+            return C.CONTENT_TYPE_DASH;
+        } else if (fileName.contains("m3u8")) {
+            return C.CONTENT_TYPE_HLS;
+        } else if (fileName.contains("isml")) {
+            return C.CONTENT_TYPE_SS;    
+        } else {
+            return C.CONTENT_TYPE_OTHER;
+        }
+    }
 
     @SuppressLint("UnsafeOptInUsageError")
     private DataSource.Factory getCacheDataSourceFactory() {
