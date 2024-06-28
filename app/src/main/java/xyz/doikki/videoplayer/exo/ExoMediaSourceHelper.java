f9a3eb7ea785d51e505ae2ccfb8892816dcba133
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 01:19:34 +0800

    Update ExoMediaSourceHelper.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -216,9 +216,7 @@ public final class ExoMediaSourceHelper implements MediaSource.Factory {
         if (dataSourceFactory == null) dataSourceFactory = buildReadOnlyCacheDataSource(new DefaultDataSource.Factory(mAppContext, getHttpDataSourceFactory()));
         return dataSourceFactory;
     }
-    private CacheDataSource.Factory buildReadOnlyCacheDataSource(DataSource.Factory upstreamFactory) {
-        return new CacheDataSource.Factory().setCache(CacheManager.get().getCache()).setUpstreamDataSourceFactory(upstreamFactory).setCacheWriteDataSinkFactory(null).setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR);
-    }
+    
     /**
      * Returns a new HttpDataSource factory.
      *
