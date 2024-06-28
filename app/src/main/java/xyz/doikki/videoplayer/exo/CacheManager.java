4d204208dc2d8cdc79b8143c761fff036c2711e6
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 00:08:17 +0800

    Update CacheManager.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/CacheManager.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/CacheManager.java
@@ -4,7 +4,7 @@ import androidx.media3.database.StandaloneDatabaseProvider;
 import androidx.media3.datasource.cache.Cache;
 import androidx.media3.datasource.cache.NoOpCacheEvictor;
 import androidx.media3.datasource.cache.SimpleCache;
-
+import androidx.media3.common.util.Util;
 import android.content.Context;
 import com.github.catvod.utils.Path;
 
@@ -12,21 +12,15 @@ public class CacheManager {
 
     private SimpleCache cache;
     private final Context mAppContext;
-    private static volatile CacheManager sInstance;
+    private final String mUserAgent;
     
     private static class Loader {
         static volatile CacheManager INSTANCE = new CacheManager();
     }
 
-    public static CacheManager getInstance(Context context) {
-        if (sInstance == null) {
-            synchronized (CacheManager.class) {
-                if (sInstance == null) {
-                    sInstance = new CacheManager(context);
-                }
-            }
-        }
-        return sInstance;
+    private CacheManager(Context context) {
+        mAppContext = context.getApplicationContext();
+        mUserAgent = Util.getUserAgent(mAppContext, mAppContext.getApplicationInfo().name);
     }
 
     public static CacheManager get() {
