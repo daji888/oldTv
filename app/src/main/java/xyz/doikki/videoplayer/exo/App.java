82b2ffc418dcf6e97fdd5ff9ea5238228e5c1187
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 16:21:19 +0800

    Update App.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/App.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/App.java
@@ -1,10 +1,91 @@
 package xyz.doikki.videoplayer.exo;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.os.HandlerCompat;
+
+import com.google.gson.Gson;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.LogAdapter;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import cat.ereza.customactivityoncrash.config.CaocConfig;
+
 
 public class App extends Application {
   private static App instance;
-  public static App get() {
+  
+  public App() {
+        instance = this;
+        executor = Executors.newFixedThreadPool(Constant.THREAD_POOL * 2);
+        handler = HandlerCompat.createAsync(Looper.getMainLooper());
+        gson = new Gson();
+    }
+
+    public static App get() {
         return instance;
     }
-}  
+
+    public static Gson gson() {
+        return get().gson;
+    }
+
+    public static Activity activity() {
+        return get().activity;
+    }
+
+    public static void execute(Runnable runnable) {
+        get().executor.execute(runnable);
+    }
+
+    public static void post(Runnable runnable) {
+        get().handler.post(runnable);
+    }
+
+    public static void post(Runnable runnable, long delayMillis) {
+        get().handler.removeCallbacks(runnable);
+        if (delayMillis >= 0) get().handler.postDelayed(runnable, delayMillis);
+    }
+
+    public static void removeCallbacks(Runnable runnable) {
+        get().handler.removeCallbacks(runnable);
+    }
+
+    public static void removeCallbacks(Runnable... runnable) {
+        for (Runnable r : runnable) get().handler.removeCallbacks(r);
+    }
+
+    public void setHook(boolean hook) {
+        this.hook = hook;
+    }
+
+    private void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    private LogAdapter getLogAdapter() {
+        return new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().methodCount(0).showThreadInfo(false).tag("").build()) {
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                return true;
+            }
+        };
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Init.set(base);
+    }
