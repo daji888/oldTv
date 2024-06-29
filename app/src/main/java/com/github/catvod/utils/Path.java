8791b574920e056a5e55cababed0f50ae6f38bb6
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 20:12:26 +0800

    Update Path.java

--- app/src/main/java/com/github/catvod/utils/Path.java
+++ app/src/main/java/com/github/catvod/utils/Path.java
@@ -37,8 +37,21 @@ public class Path {
         return Environment.getExternalStorageDirectory();
     }
 
+    public static File download() {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    }
+
     public static File cache() {
         return Init.context().getCacheDir();
+    }
+
+    public static File thunderCache() {
+        File internal = Init.context().getCacheDir();
+        String dir = Prefers.getString("thunder_cache_dir", internal.getAbsolutePath());
+        if (dir.equals(internal.getAbsolutePath())) return internal;
+        File cache = new File(dir);
+        if (!cache.exists()) return internal;
+        return cache;
     }
 
     public static File files() {
@@ -51,6 +64,10 @@ public class Path {
 
     public static File externalCache() {
         return Init.context().getExternalCacheDir();
+    }
+
+    public static String downloadPath() {
+        return download().getAbsolutePath();
     }
 
     public static String rootPath() {
@@ -81,12 +98,24 @@ public class Path {
         return check(new File(cache() + File.separator + "exo"));
     }
 
+    public static File epg() {
+        return check(new File(cache() + File.separator + "epg"));
+    }
+
     public static File jpa() {
         return check(new File(cache() + File.separator + "jpa"));
     }
 
     public static File thunder() {
-        return check(new File(cache() + File.separator + "thunder"));
+        return check(new File(thunderCache() + File.separator + "thunder"));
+    }
+
+    public static File restore() {
+        return check(new File(cache() + File.separator + "restore"));
+    }
+
+    public static File epg(String name) {
+        return new File(epg(), name);
     }
 
     public static File root(String name) {
@@ -157,24 +186,42 @@ public class Path {
             byte[] data = new byte[is.available()];
             is.read(data);
             is.close();
-            return new String(data, StandardCharsets.UTF_8);
+            return new String(data, "UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
             return "";
         }
     }
 
+    public static byte[] readToByte(File file) {
+        try {
+            FileInputStream is = new FileInputStream(file);
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            is.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
     public static File write(File file, byte[] data) {
         try {
-            FileOutputStream fos = new FileOutputStream(file);
+            FileOutputStream fos = new FileOutputStream(create(file));
             fos.write(data);
             fos.flush();
             fos.close();
             chmod(file);
             return file;
         } catch (Exception ignored) {
+            ignored.printStackTrace();
             return file;
         }
+    }
+
+    public static File utf8(File file) {
+        return write(cache(file.getName()), Util.utf8(readToByte(file)));
     }
 
     public static void move(File in, File out) {
@@ -184,14 +231,19 @@ public class Path {
 
     public static void copy(File in, File out) {
         try {
-            copy(new FileInputStream(in), new FileOutputStream(out));
+            copy(new FileInputStream(in), out);
         } catch (Exception ignored) {
         }
     }
 
     public static void copy(InputStream in, File out) {
         try {
-            copy(in, new FileOutputStream(out));
+            int read;
+            byte[] buffer = new byte[8192];
+            FileOutputStream fos = new FileOutputStream(create(out));
+            while ((read = in.read(buffer)) != -1) fos.write(buffer, 0, read);
+            fos.close();
+            in.close();
         } catch (Exception ignored) {
         }
     }
@@ -233,6 +285,17 @@ public class Path {
         try {
             Process process = Runtime.getRuntime().exec("chmod 777 " + file);
             process.waitFor();
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return file;
+        }
+    }
+    public static File create(File file) throws Exception {
+        try {
+            if (!file.canWrite()) file.setWritable(true);
+            if (!file.exists()) file.createNewFile();
+            Shell.exec("chmod 777 " + file);
             return file;
         } catch (Exception e) {
             e.printStackTrace();
