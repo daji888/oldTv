c000d5ea917d4ed34ec1e498c23c2ae4b29472dc
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 15:16:00 +0800

    Create CacheManager.java

---
+++ app/src/main/java/xyz/doikki/videoplayer/exo/CacheManager.java
@@ -0,0 +1,32 @@
+package xyz.doikki.videoplayer.exo;
+
+import androidx.media3.database.StandaloneDatabaseProvider;
+import androidx.media3.datasource.cache.Cache;
+import androidx.media3.datasource.cache.NoOpCacheEvictor;
+import androidx.media3.datasource.cache.SimpleCache;
+
+import android.content.Context;
+import com.github.catvod.utils.Path;
+
+public class CacheManager {
+
+    private SimpleCache cache;
+    private final Context mAppContext;
+
+    private static class Loader {
+        static volatile CacheManager INSTANCE = new CacheManager();
+    }
+
+    public static CacheManager get() {
+        return Loader.INSTANCE;
+    }
+
+    public Cache getCache() {
+        if (cache == null) create();
+        return cache;
+    }
+
+    private void create() {
+        cache = new SimpleCache(Path.exo(), new NoOpCacheEvictor(), new StandaloneDatabaseProvider(mAppContext));
+    }
+}
