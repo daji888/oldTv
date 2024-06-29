d550b3c0d1542c69615f31fefd617459a312aea1
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-29 16:01:40 +0800

    Update CacheManager.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/CacheManager.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/CacheManager.java
@@ -1,22 +1,15 @@
 package xyz.doikki.videoplayer.exo;
 
-import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor;
 import androidx.media3.database.StandaloneDatabaseProvider;
 import androidx.media3.datasource.cache.Cache;
 import androidx.media3.datasource.cache.NoOpCacheEvictor;
 import androidx.media3.datasource.cache.SimpleCache;
 
-import android.content.Context;
 import com.github.catvod.utils.Path;
-import com.github.tvbox.osc.util.FileUtils;
 
 public class CacheManager {
 
     private SimpleCache cache;
-    private Context mAppContext;
-    public CacheManager(Context context) {
-        mAppContext = context.getApplicationContext();
-    }
 
     private static class Loader {
         static volatile CacheManager INSTANCE = new CacheManager();
@@ -32,6 +25,6 @@ public class CacheManager {
     }
 
     private void create() {
-        cache = new SimpleCache(File(FileUtils.getExternalCachePath(), "exo-video-cache"), new LeastRecentlyUsedCacheEvictor(512 * 1024 * 1024), new StandaloneDatabaseProvider(mAppContext));
+        cache = new SimpleCache(Path.exo(), new NoOpCacheEvictor(), new StandaloneDatabaseProvider(App.get()));
     }
 }
