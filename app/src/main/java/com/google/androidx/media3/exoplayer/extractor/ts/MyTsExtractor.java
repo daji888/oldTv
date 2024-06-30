c6aa0fc044cf94a6b31c776e532e31d460513cf6
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-30 11:00:23 +0800

    Update MyTsExtractor.java

--- app/src/main/java/com/google/androidx/media3/exoplayer/extractor/ts/MyTsExtractor.java
+++ app/src/main/java/com/google/androidx/media3/exoplayer/extractor/ts/MyTsExtractor.java
@@ -18,6 +18,9 @@ package com.google.androidx.media3.exoplayer.extractor.ts;
 import static androidx.media3.extractor.ts.TsPayloadReader.EsInfo.AUDIO_TYPE_UNDEFINED;
 import static androidx.media3.extractor.ts.TsPayloadReader.FLAG_PAYLOAD_UNIT_START_INDICATOR;
 
+import androidx.media3.extractor.ts.TsBinarySearchSeeker;
+import androidx.media3.extractor.ts.TsDurationReader;
+
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
 import android.util.SparseIntArray;
