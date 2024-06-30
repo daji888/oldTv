ab436c81bed43ce0457a2775cd063338d3203154
daji888 <97334834+daji888@users.noreply.github.com>
2024-06-30 15:09:56 +0800

    Update MyHlsExtractorFactory.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/MyHlsExtractorFactory.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/MyHlsExtractorFactory.java
@@ -174,7 +174,7 @@ public final class MyHlsExtractorFactory implements HlsExtractorFactory {
     }
 
     @Override
-    public MyBundledHlsMediaChunkExtractor createExtractor(
+    public BundledHlsMediaChunkExtractor createExtractor(
             Uri uri,
             Format format,
             @Nullable List<Format> muxedCaptionFormats,
@@ -209,7 +209,7 @@ public final class MyHlsExtractorFactory implements HlsExtractorFactory {
                     (Extractor) checkNotNull(
                             createExtractorByFileType(fileType, format, muxedCaptionFormats, timestampAdjuster));
             if (sniffQuietly(extractor, sniffingExtractorInput)) {
-                return new MyBundledHlsMediaChunkExtractor(extractor, format, timestampAdjuster);
+                return new BundledHlsMediaChunkExtractor(extractor, format, timestampAdjuster);
             }
             if (fallBackExtractor == null
                     && (fileType == formatInferredFileType
@@ -222,7 +222,7 @@ public final class MyHlsExtractorFactory implements HlsExtractorFactory {
             }
         }
 
-        return new MyBundledHlsMediaChunkExtractor(
+        return new BundledHlsMediaChunkExtractor(
                 checkNotNull(fallBackExtractor), format, timestampAdjuster);
     }
 
