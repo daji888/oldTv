5edfbc8989c732b881ae0b898973240daf5b5e5e
daji888 <97334834+daji888@users.noreply.github.com>
2024-07-01 23:24:55 +0800

    Update Util.java

--- app/src/main/java/com/github/catvod/utils/Util.java
+++ app/src/main/java/com/github/catvod/utils/Util.java
@@ -52,6 +52,14 @@ public class Util {
         return Base64.encodeToString(bytes, Base64.DEFAULT | Base64.NO_WRAP);
     }
 
+    public static String base64(String ext, int flags) {
+        return base64(ext.getBytes(), flags);
+    }
+
+    public static String base64(byte[] bytes, int flags) {
+        return Base64.encodeToString(bytes, flags);
+    }
+
     public static String substring(String text) {
         return substring(text, 1);
     }
