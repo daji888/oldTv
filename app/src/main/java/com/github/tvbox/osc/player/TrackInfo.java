e15e1485fb423248dcd438052cf19ecfe32df77b
daji888 <97334834+daji888@users.noreply.github.com>
2024-07-02 10:28:09 +0800

    Update TrackInfo.java

--- app/src/main/java/com/github/tvbox/osc/player/TrackInfo.java
+++ app/src/main/java/com/github/tvbox/osc/player/TrackInfo.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class TrackInfo {
     private final List<TrackInfoBean> audio;
     private final List<TrackInfoBean> subtitle;
+    private final List<TrackInfoBean> language;
 
     public TrackInfo() {
         audio = new ArrayList<>();
         subtitle = new ArrayList<>();
+        language = new ArrayList<>();
     }
 
     public List<TrackInfoBean> getAudio() {
@@ -19,13 +21,21 @@ public class TrackInfo {
     public List<TrackInfoBean> getSubtitle() {
         return subtitle;
     }
-
+    
+    public List<TrackInfoBean> getLanguage() {
+        return language;
+    }
+    
     public int getAudioSelected(boolean track) {
         return getSelected(audio, track);
     }
 
     public int getSubtitleSelected(boolean track) {
         return getSelected(subtitle, track);
+    }
+
+    public int getLanguageSelected(boolean track) {
+        return getSelected(language, track);
     }
 
     public int getSelected(List<TrackInfoBean> list, boolean track) {
@@ -43,5 +53,9 @@ public class TrackInfo {
 
     public void addSubtitle(TrackInfoBean subtitle) {
         this.subtitle.add(subtitle);
+    }
+
+    public void addLanguage(TrackInfoBean language) {
+        this.language.add(language);
     }
 }
