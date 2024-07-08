80836d1b767f52d807eaa5b972d738789aaa2d63
daji888 <97334834+daji888@users.noreply.github.com>
2024-07-08 17:22:18 +0800

    Update OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
