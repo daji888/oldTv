f0f778d8166f89c15efa9bee3e7365901b5f9456
daji888 <97334834+daji888@users.noreply.github.com>
2024-08-14 22:50:28 +0800

    Update ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -92,7 +92,7 @@ public class ApiDialog extends BaseDialog {
                 if (history.contains(current))
                     idx = history.indexOf(current);
                 ApiHistoryDialog dialog = new ApiHistoryDialog(getContext());
-                dialog.setTip("历史配置列表");
+                dialog.setTip("配置历史列表");
                 dialog.setAdapter(new ApiHistoryDialogAdapter.SelectDialogInterface() {
                     @Override
                     public void click(String value) {
