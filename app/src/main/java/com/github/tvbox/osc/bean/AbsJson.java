073e970b8389fd8aa7c35df77deb1f78a2af930d
daji888 <97334834+daji888@users.noreply.github.com>
2024-08-18 18:07:19 +0800

    Update AbsJson.java

--- app/src/main/java/com/github/tvbox/osc/bean/AbsJson.java
+++ app/src/main/java/com/github/tvbox/osc/bean/AbsJson.java
@@ -127,7 +127,7 @@ public class AbsJson implements Serializable {
             video.actor = vod_actor;
             video.director = vod_director;
             Movie.Video.UrlBean urlBean = new Movie.Video.UrlBean();
-            if (vod_play_from != null && vod_play_url != null) {
+            if (!TextUtils.isEmpty(vod_play_from) && !TextUtils.isEmpty(vod_play_url)) {
                 String[] playFlags = vod_play_from.split("\\$\\$\\$");
                 String[] playUrls = vod_play_url.split("\\$\\$\\$");
                 List<Movie.Video.UrlBean.UrlInfo> infoList = new ArrayList<>();
