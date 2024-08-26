049940183f8cf74626a20ecfe6d5d2eb065541bf
daji888 <97334834+daji888@users.noreply.github.com>
2024-08-26 13:40:57 +0800

    Update HistoryActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HistoryActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HistoryActivity.java
@@ -146,7 +146,7 @@ public class HistoryActivity extends BaseActivity {
         List<VodInfo> allVodRecord = RoomDataManger.getAllVodRecord(50);
         List<VodInfo> vodInfoList = new ArrayList<>();
         for (VodInfo vodInfo : allVodRecord) {
-            if (vodInfo.playNote != null && !vodInfo.playNote.isEmpty())vodInfo.note = "上次看到" + vodInfo.playNote;
+            if (vodInfo.playNote != null && !vodInfo.playNote.isEmpty())vodInfo.note = "上次看到：" + vodInfo.playNote;
             vodInfoList.add(vodInfo);
         }
         historyAdapter.setNewData(vodInfoList);
