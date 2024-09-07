0b57c5ed658e5f0aba6b1b964b3cb35d0248f10d
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-07 15:12:03 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -67,7 +67,6 @@ public class ApiConfig {
     private List<String> vipParseFlags;
     private List<IJKCode> ijkCodes;
     private List<EXOCode> exoCodes;
-    private List<EXOCode> exoCodes;
     private String spider = null;
     public String wallpaper = "";
 
