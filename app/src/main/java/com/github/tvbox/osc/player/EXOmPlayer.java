f30d4140e9fb0aab0933765e256da2b64b2ead8f
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-10 15:24:37 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -44,9 +44,9 @@ public class EXOmPlayer extends ExoMediaPlayer {
                 String name = opt[1].trim();
                 try {
                     long valLong = Long.parseLong(value);
-                    mMediaPlayer.Builder.setRenderersFactory(mRenderersFactory);
+                    mMediaPlayer.setRenderersFactory(mRenderersFactory);
                 } catch (Exception e) {
-                    mMediaPlayer.Builder.setRenderersFactory(mRenderersFactory);
+                    mMediaPlayer.setRenderersFactory(mRenderersFactory);
                 }
             }
         }
