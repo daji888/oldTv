783c21fbd73fe9e8e72133c48c9ddd5ca4fcbaf0
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-11 19:51:26 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -72,8 +72,8 @@ public class EXOmPlayer extends ExoMediaPlayer {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }*/
-        }
+            }
+        }*/
         super.setOptions();
     }
 
