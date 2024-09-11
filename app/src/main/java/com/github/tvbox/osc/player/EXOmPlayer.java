a4c658784fd9f3d13d9faafa18734ce7e6e37bc0
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-11 15:50:01 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -56,7 +56,7 @@ public class EXOmPlayer extends ExoMediaPlayer {
                 try {
                     if (mRenderersFactory == null) {
                         mRenderersFactory = new DefaultRenderersFactory(mAppContext);
-                        mRenderersFactory.setExtensionRendererMode(DefaultRenderersFactory.extensionRendererMode);
+                        mRenderersFactory.setExtensionRendererMode(extensionRendererMode);
                      }   
                  /*   if (mRenderersFactory == null) {
                         mRenderersFactory = new DefaultRenderersFactory(mAppContext);
