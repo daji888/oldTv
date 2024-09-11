7123ad10a9d31ac424ad697959e641360ab6f613
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-11 17:25:15 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -44,7 +44,7 @@ public class EXOmPlayer extends ExoMediaPlayer {
     private @ExtensionRendererMode int extensionRendererMode;
 
     @Override
-    public void setOptions() {
+    public void setOptions(extensionRendererMode) {
         EXOCode exocodecTmp = this.exocodec == null ? ApiConfig.get().getCurrentEXOCode() : this.exocodec;
         LinkedHashMap<String, String> options = exocodecTmp.getOption();
         if (options != null) {
@@ -86,7 +86,7 @@ public class EXOmPlayer extends ExoMediaPlayer {
                             mRenderersFactory.setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);
                       }   
                 //   } 
-        super.setOptions();
+        super.setOptions(extensionRendererMode);
     }
 
     @SuppressLint("UnsafeOptInUsageError")
