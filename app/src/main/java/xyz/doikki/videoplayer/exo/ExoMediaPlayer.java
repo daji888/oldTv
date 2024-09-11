8f1046ee3ab510c0a92c4c65ea8c023febf478c4
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-11 17:32:55 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -121,7 +121,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         }*/
         if (mRenderersFactory == null) {
             mRenderersFactory = new DefaultRenderersFactory(mAppContext);
-            setOptions(extensionRendererMode);
+            setOptions();
         }    
         //https://github.com/androidx/media/blob/release/libraries/decoder_ffmpeg/README.md
         if ("MiTV-MFTR0".equals(Build.MODEL)) {
