c983f61356c3bc1fe2dd21d25a0d4d48ce62487b
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-11 23:34:43 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -73,7 +73,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
     @SuppressLint("UnsafeOptInUsageError")
     @Override
     public void initPlayer() {
-        if (mRenderersFactory == null) {
+    /*    if (mRenderersFactory == null) {
             mRenderersFactory = new DefaultRenderersFactory(mAppContext);
             EXOCode exocodecTmp = ApiConfig.get().getCurrentEXOCode();
             LinkedHashMap<String, String> options = exocodecTmp.getOption();
@@ -94,6 +94,9 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
                       }
                  }
             }
+        }*/
+        if (mRenderersFactory == null) {
+            mRenderersFactory = new DefaultRenderersFactory(mAppContext);
         }
         //https://github.com/androidx/media/blob/release/libraries/decoder_ffmpeg/README.md
         if ("MiTV-MFTR0".equals(Build.MODEL)) {
