54e119dcd4fb512461a8cb61fae5e42efb961718
daji888 <97334834+daji888@users.noreply.github.com>
2024-09-12 16:01:51 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -866,6 +866,7 @@ public class VodController extends BaseController {
                 mPlayerDecodeBtn.setText("硬解");
                 mPlayerDecodeBtn.setVisibility(VISIBLE);
                 mPlayerDecodeBtn.setFocusable(false);
+                mPlayerDecodeBtn.setFocusableInTouchMode(false);
             }
             mPlayerSpeedBtn.setText("x" + mPlayerConfig.getDouble("sp"));
             mPlayerTimeStartBtn.setText(PlayerUtils.stringForTime(mPlayerConfig.getInt("st") * 1000));
