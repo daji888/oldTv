e4eb412758aacc61a5e841753aafa3e7b694d017
daji888 <97334834+daji888@users.noreply.github.com>
2025-02-23 16:00:29 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -102,6 +102,7 @@ import org.xwalk.core.XWalkWebResourceResponse;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -145,6 +146,7 @@ public class PlayActivity extends BaseActivity {
         initView();
         initViewModel();
         initData();
+        Hawk.put(HawkConfig.PLAYER_IS_LIVE,false);
     }
 
     public long getSavedProgress(String url) {
@@ -257,7 +259,11 @@ public class PlayActivity extends BaseActivity {
             @Override
             public void replay(boolean replay) {
                 autoRetryCount = 0;
-                play(replay);
+                if (replay) {
+                    play(true);
+                } else {
+                    playUrl(webPlayUrl,webHeaderMap);
+                }
             }
 
             @Override
@@ -770,8 +776,15 @@ public class PlayActivity extends BaseActivity {
 
     void startPlayUrl(String url, HashMap<String, String> headers) {
         LOG.i("playUrl:" + url);
+        if (autoRetryCount == 0) webPlayUrl = url;
         if (autoRetryCount > 1 && url.contains(".m3u8")) {
-           // url = "http://home.jundie.top:666/unBom.php?m3u8=" + url;//尝试去bom头再次播放
+           try {
+                String url_encode;
+                url_encode=URLEncoder.encode(url,"UTF-8");
+                url = ControlManager.get().getAddress(true) + "proxy?go=bom&url="+ url_encode;
+            } catch (UnsupportedEncodingException e) {
+
+            }
         }
         final String finalUrl = url;
         runOnUiThread(new Runnable() {
@@ -949,6 +962,7 @@ public class PlayActivity extends BaseActivity {
                         HashMap<String, String> headers = null;
                         webUserAgent = null;
                         webHeaderMap = null;
+                        webPlayUrl = null;
                         if (info.has("header")) {
                             try {
                                 JSONObject hds = new JSONObject(info.getString("header"));
@@ -1160,16 +1174,31 @@ public class PlayActivity extends BaseActivity {
     }
 
     private int autoRetryCount = 0;
+    
+    private long lastRetryTime = 0;  // 记录上次调用时间（毫秒）
 
     boolean autoRetry() {
         switchPlayer();
-        if (loadFoundVideoUrls != null && loadFoundVideoUrls.size() > 0) {
+        long currentTime = System.currentTimeMillis();
+        // 如果距离上次重试超过 10 秒（10000 毫秒），重置重试次数
+        if (currentTime - lastRetryTime > 10_000) {
+            autoRetryCount = 0;
+        }
+        lastRetryTime = currentTime;  // 更新上次调用时间
+        if (loadFoundVideoUrls != null && !loadFoundVideoUrls.isEmpty()) {
             autoRetryFromLoadFoundVideoUrls();
             return true;
         }
-        if (autoRetryCount < 1) {
+        if (autoRetryCount < 2) {
+            if (autoRetryCount == 1) {
+                //第二次重试时重新调用接口
+                play(false);
+            } else {
+                //第一次重试直接带着原地址继续播放
+                playUrl(webPlayUrl, webHeaderMap);
+            }
             autoRetryCount++;
-            play(false);
+    //        play(false);
             return true;
         } else {
             autoRetryCount = 0;
@@ -1271,7 +1300,8 @@ public class PlayActivity extends BaseActivity {
     private String parseFlag;
     private String webUrl;
     private String webUserAgent;
-    private Map<String, String > webHeaderMap;
+    private HashMap<String, String > webHeaderMap;
+    private String webPlayUrl;
 
     private void initParse(String flag, boolean useParse, String playUrl, final String url) {
         parseFlag = flag;
