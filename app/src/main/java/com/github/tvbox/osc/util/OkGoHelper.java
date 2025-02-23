23e5574d87439b6399d031af5e18e21ebc4db40c
daji888 <97334834+daji888@users.noreply.github.com>
2025-02-23 16:05:58 +0800

    Update OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -1,11 +1,14 @@
 package com.github.tvbox.osc.util;
 
+import androidx.annotation.NonNull;
+
 import static okhttp3.ConnectionSpec.CLEARTEXT;
 import static okhttp3.ConnectionSpec.COMPATIBLE_TLS;
 import static okhttp3.ConnectionSpec.MODERN_TLS;
 import static okhttp3.ConnectionSpec.RESTRICTED_TLS;
 import com.github.catvod.net.SSLCompat;
 import com.github.tvbox.osc.base.App;
+import com.github.tvbox.osc.api.ApiConfig;
 
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.https.HttpsUtils;
@@ -14,19 +17,27 @@ import com.lzy.okgo.model.HttpHeaders;
 import com.orhanobut.hawk.Hawk;
 
 import java.io.File;
-import java.util.List;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
 import okhttp3.Cache;
+import okhttp3.Dns;
 import okhttp3.ConnectionSpec;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -53,6 +64,8 @@ public class OkGoHelper {
         put(504,"Gateway Timeout");
     }};
 
+    static OkHttpClient ItvClient = null;
+
     static void initExoOkHttpClient() {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkExoPlayer");
@@ -76,14 +89,20 @@ public class OkGoHelper {
         } catch (Throwable th) {
             th.printStackTrace();
         }
-        builder.dns(dnsOverHttps);
+//        builder.dns(dnsOverHttps);
+        builder.dns(new CustomDns());
+        ItvClient=builder.build();
 
-        ExoMediaSourceHelper.getInstance(App.getInstance()).setOkClient(builder.build());
+        ExoMediaSourceHelper.getInstance(App.getInstance()).setOkClient(ItvClient);
     }
 
     public static DnsOverHttps dnsOverHttps = null;
 
     public static ArrayList<String> dnsHttpsList = new ArrayList<>();
+
+    public static boolean is_doh = false;
+    
+    public static Map<String,String> myHosts = null;
 
     public static List<ConnectionSpec> getConnectionSpec() {
         return Util.immutableListOf(RESTRICTED_TLS, MODERN_TLS, COMPATIBLE_TLS, CLEARTEXT);
@@ -142,7 +161,85 @@ public class OkGoHelper {
         builder.cache(new Cache(new File(App.getInstance().getCacheDir().getAbsolutePath(), "dohcache"), 10 * 1024 * 1024));
         OkHttpClient dohClient = builder.build();
         String dohUrl = getDohUrl(Hawk.get(HawkConfig.DOH_URL, 0));
+        if (!dohUrl.isEmpty()) is_doh = true;
         dnsOverHttps = new DnsOverHttps.Builder().client(dohClient).url(dohUrl.isEmpty() ? null : HttpUrl.get(dohUrl)).build();
+    }
+
+    // 自定义 DNS 解析器
+    static class CustomDns implements Dns {
+        private  ConcurrentHashMap<String, List<InetAddress>> map;
+        private final String excludeIps = "2409:8087:6c02:14:100::14,2409:8087:6c02:14:100::18,39.134.108.253,39.134.108.245";
+        @NonNull
+        @Override
+        public List<InetAddress> lookup(@NonNull String hostname) throws UnknownHostException {
+            if (myHosts == null){
+                myHosts = ApiConfig.get().getMyHost(); //确保只获取一次减少消耗
+                if(!myHosts.isEmpty())mapHosts(myHosts);
+            }
+            // 判断输入是否为 IP 地址
+            if (isValidIpAddress(hostname)) {
+                return Collections.singletonList(InetAddress.getByName(hostname));
+            } else if (!map.isEmpty() && map.containsKey(hostname)) {
+                return Objects.requireNonNull(map.get(hostname));    
+            } else {
+//                return (is_doh?dnsOverHttps:Dns.SYSTEM).lookup(hostname);
+                return (dnsOverHttps).lookup(hostname);
+            }
+        }
+        
+        public synchronized void mapHosts(Map<String,String> hosts) {
+            map=new ConcurrentHashMap<>();
+            for (Map.Entry<String, String> entry : hosts.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                map.put(key,getAllByName(value));
+            }
+        }
+
+        private List<InetAddress> getAllByName(String host) {
+            try {
+                // 获取所有与主机名关联的 IP 地址
+                InetAddress[] allAddresses = InetAddress.getAllByName(host);
+                // 创建一个列表用于存储有效的 IP 地址
+                List<InetAddress> validAddresses = new ArrayList<>();
+                Set<String> excludeIpsSet = new HashSet<>();
+                for (String ip : excludeIps.split(",")) {
+                    excludeIpsSet.add(ip.trim());  // 添加到集合，去除多余的空格
+                }
+                for (InetAddress address : allAddresses) {
+                    if (!excludeIpsSet.contains(address.getHostAddress())) {
+                        validAddresses.add(address);
+                    }
+                }
+                return validAddresses;
+            } catch (Exception e) {
+                return new ArrayList<>();
+            }
+        }
+
+        //简单判断减少开销
+        private boolean isValidIpAddress(String str) {
+            // 处理 IPv4 地址的判断
+            if (str.indexOf('.') > 0) return isValidIPv4(str);
+            // 处理 IPv6 地址的判断
+            if (str.indexOf(':') > 0) return true;
+            return false;
+        }
+
+        private boolean isValidIPv4(String str) {
+            String[] parts = str.split("\\.");
+            // IPv4 地址必须有 4 个部分
+            if (parts.length != 4) return false;
+            // 检查每一部分是否是 0-255 的数字
+            for (String part : parts) {
+                try {
+                    Integer.parseInt(part);
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     static OkHttpClient defaultClient = null;
