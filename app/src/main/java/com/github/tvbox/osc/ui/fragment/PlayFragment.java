513abd3896fc241f7ec4aef9c7f91601a5731cf1
daji888 <97334834+daji888@users.noreply.github.com>
2025-02-23 22:55:18 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -283,7 +283,11 @@ public class PlayFragment extends BaseLazyFragment {
                 if (replay) {
                     play(true);
                 } else {
-                    playUrl(webPlayUrl,webHeaderMap);
+                    if (webPlayUrl != null && !webPlayUrl.isEmpty()) {
+                        playUrl(webPlayUrl,webHeaderMap);
+                    } else {
+                        play(false);
+                    }
                 }
             }
 
