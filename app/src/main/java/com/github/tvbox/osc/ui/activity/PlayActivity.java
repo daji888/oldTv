ecf66c42a4ef4af39f6e299dec466e46e512f521
daji888 <97334834+daji888@users.noreply.github.com>
2025-02-28 22:05:27 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -102,8 +102,6 @@ import org.xwalk.core.XWalkWebResourceResponse;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -259,6 +257,7 @@ public class PlayActivity extends BaseActivity {
             @Override
             public void replay(boolean replay) {
                 autoRetryCount = 0;
+                LOG.i("echo-replay");
                 if (replay) {
                     play(true);
                 } else {
@@ -781,16 +780,6 @@ public class PlayActivity extends BaseActivity {
     void startPlayUrl(String url, HashMap<String, String> headers) {
         LOG.i("playUrl:" + url);
         if (autoRetryCount == 0) webPlayUrl = url;
-        if (autoRetryCount > 1 && url.contains(".m3u8")) {
-           try {
-                String url_encode;
-                url_encode=URLEncoder.encode(url,"UTF-8");
-                LOG.i("echo-BOM-------");
-                url = ControlManager.get().getAddress(true) + "proxy?go=bom&url="+ url_encode;
-            } catch (UnsupportedEncodingException e) {
-
-            }
-        }
         final String finalUrl = url;
         runOnUiThread(new Runnable() {
             @Override
@@ -950,7 +939,7 @@ public class PlayActivity extends BaseActivity {
                                             break;
                                     }
                                     String filename = name + (name.toLowerCase().endsWith(ext) ? "" : ext);
-                                    url += "#" + URLEncoder.encode(filename);
+                                    url += "#" + mController.encodeUrl(filename);
                                 }
                                 playSubtitle = url;
                             } catch (Throwable th) {
@@ -1000,7 +989,8 @@ public class PlayActivity extends BaseActivity {
                     } catch (Throwable th) {
                     }
                 } else {
-                    errorWithRetry("获取播放信息错误", true);
+                    //获取播放信息错误后只需再重试一次
+                    setTip("获取播放信息错误", false, true);
                 }
             }
         });
@@ -1183,10 +1173,9 @@ public class PlayActivity extends BaseActivity {
     private long lastRetryTime = 0;  // 记录上次调用时间（毫秒）
 
     boolean autoRetry() {
-        switchPlayer();
         long currentTime = System.currentTimeMillis();
         // 如果距离上次重试超过 10 秒（10000 毫秒），重置重试次数
-        if (currentTime - lastRetryTime > 10_000) {
+        if (autoRetryCount < 2 && currentTime - lastRetryTime > 10_000) {
             autoRetryCount = 0;
         }
         lastRetryTime = currentTime;  // 更新上次调用时间
@@ -1198,28 +1187,25 @@ public class PlayActivity extends BaseActivity {
             if (autoRetryCount == 1) {
                 //第二次重试时重新调用接口
                 play(false);
+                autoRetryCount++;
             } else {
+                if (mController.switchPlayer()) {
+                    autoRetryCount++;
+                    webPlayUrl=mController.getWebPlayUrlIfNeeded(webPlayUrl);
+                } else {
+//                    Toast.makeText(mContext, "自动切换播放器重试", Toast.LENGTH_SHORT).show();
+                }
                 //第一次重试直接带着原地址继续播放
-                playUrl(webPlayUrl, webHeaderMap);
+                if (webPlayUrl != null) {
+                    playUrl(webPlayUrl, webHeaderMap);
+                } else {
+                    play(false);
+                }
             }
-            autoRetryCount++;
-    //        play(false);
             return true;
         } else {
             autoRetryCount = 0;
             return false;
-        }
-    }
-
-    void switchPlayer() {
-        try {
-            int playerType = mVodPlayerCfg.getInt("pl") == 1 ? 2 : 1;
-            mVodPlayerCfg.put("pl", playerType);
-            mController.setPlayerConfig(mVodPlayerCfg);
-            mVodInfo.playerCfg = mVodPlayerCfg.toString();
-            EventBus.getDefault().post(new RefreshEvent(RefreshEvent.TYPE_REFRESH, mVodPlayerCfg));
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -1383,14 +1369,6 @@ public class PlayActivity extends BaseActivity {
 
     ExecutorService parseThreadPool;
 
-    private String encodeUrl(String url) {
-        try {
-            return URLEncoder.encode(url, "UTF-8");
-        } catch (Exception e) {
-            return url;
-        }
-    }
-
     private void doParse(ParseBean pb) {
         stopParse();
         initParseLoadFound();
@@ -1398,7 +1376,7 @@ public class PlayActivity extends BaseActivity {
             setTip("正在嗅探播放地址", true, false);
             mHandler.removeMessages(100);
             mHandler.sendEmptyMessageDelayed(100, 20 * 1000);
-            if(pb.getExt()!=null){
+            if (pb.getExt() != null) {
                 // 解析ext
                 try {
                     HashMap<String, String> reqHeaders = new HashMap<>();
@@ -1410,11 +1388,11 @@ public class PlayActivity extends BaseActivity {
                             String key = keys.next();
                             if (key.equalsIgnoreCase("user-agent")) {
                                 webUserAgent = headerJson.getString(key).trim();
-                            }else {
+                            } else {
                                 reqHeaders.put(key, headerJson.optString(key, ""));
                             }
                         }
-                        if(reqHeaders.size()>0)webHeaderMap = reqHeaders;
+                        if (reqHeaders.size() > 0) webHeaderMap = reqHeaders;
                     }
                 } catch (Throwable e) {
                     e.printStackTrace();
@@ -1438,7 +1416,7 @@ public class PlayActivity extends BaseActivity {
             } catch (Throwable e) {
                 e.printStackTrace();
             }
-            OkGo.<String>get(pb.getUrl() + encodeUrl(webUrl))
+            OkGo.<String>get(pb.getUrl() + mController.encodeUrl(webUrl))
                     .tag("json_jx")
                     .headers(reqHeaders)
                     .execute(new AbsCallback<String>() {
