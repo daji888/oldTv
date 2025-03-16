ca177e7f28f6f5e713eb6b50c2f116c6cc55862d
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-16 15:43:32 +0800

    Update ImgUtil.java

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -2,7 +2,9 @@ package com.github.tvbox.osc.util;
 
 import static com.bumptech.glide.load.resource.bitmap.VideoDecoder.FRAME_OPTION;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
+import android.util.Base64;
 import android.text.TextUtils;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
@@ -60,6 +62,17 @@ public class ImgUtil {
 //                .into(view);
 //        }
 //    }
+
+    public static boolean isBase64Image(String picUrl) {
+         return picUrl.startsWith("data:image");
+     }
+ 
+     public static Bitmap decodeBase64ToBitmap(String base64Str) {
+         // 去掉 Base64 数据的头部前缀，例如 "data:image/png;base64,"
+         String base64Data = base64Str.substring(base64Str.indexOf(",") + 1);
+         byte[] decodedBytes = Base64.decode(base64Data, Base64.DEFAULT);
+         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+     }
 
     public static void load(String url, ImageView view, int roundingRadius) {
         view.setScaleType(ImageView.ScaleType.CENTER);
