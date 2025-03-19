3a0b154ddff5977f54cae1c561cf25da9200887a
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-19 16:59:33 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -991,6 +991,9 @@ public class PlayFragment extends BaseLazyFragment {
                         }
                         String flag = info.optString("flag");
                         String url = info.getString("url");
+                        if (url.startsWith("[")) {
+                             url = mController.firstUrlByArray(url);
+                         }
                         HashMap<String, String> headers = null;
                         webUserAgent = null;
                         webHeaderMap = null;
