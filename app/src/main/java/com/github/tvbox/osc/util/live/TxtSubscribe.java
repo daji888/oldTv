9eae607776742c4dd58ada032dac432eea7ff2fb
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-20 14:27:47 +0800

    Update TxtSubscribe.java

--- app/src/main/java/com/github/tvbox/osc/util/live/TxtSubscribe.java
+++ app/src/main/java/com/github/tvbox/osc/util/live/TxtSubscribe.java
@@ -87,10 +87,9 @@ public class TxtSubscribe {
             LinkedHashMap<String, ArrayList<String>> linkedHashMap2 = new LinkedHashMap<>();
             LinkedHashMap<String, ArrayList<String>> linkedHashMap3 = linkedHashMap2;
             while (readLine != null) {
-                if (readLine.trim().isEmpty()) {
+                if (readLine.trim().isEmpty() || readLine.startsWith("#")) {
                     readLine = bufferedReader.readLine();
                 } else {
-                    if (readLine.startsWith("#")) continue;
                     String[] split = readLine.split(",", 2);
                     if (split.length < 2) {
                         readLine = bufferedReader.readLine();
