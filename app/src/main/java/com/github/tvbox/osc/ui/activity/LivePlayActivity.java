33d19cca60757efe0a239bb62e968ec0a120c841
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-20 14:47:50 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1989,7 +1989,7 @@ public class LivePlayActivity extends BaseActivity {
         @Override
         public void run() {
             if (mVideoView == null) return;
-            String speed = PlayerHelper.getDisplaySpeed(mVideoView.getTcpSpeed());
+            String speed = PlayerHelper.getDisplaySpeed(mVideoView.getTcpSpeed(), true);
             tv_right_top_tipnetspeed.setText(speed);
             mHandler.postDelayed(this, 1000);
         }
