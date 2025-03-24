62225e3904d6b74a099f81d1dc3869ac8f066964
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-24 18:42:44 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -843,7 +843,6 @@ public class ModelSettingFragment extends BaseLazyFragment {
             try {
                 if (cacheDir.exists()) FileUtils.cleanDirectory(cacheDir);
                 if (cspCacheDir.exists()) {
-                     ApiConfig.get().clearJarLoader();
                      FileUtils.cleanDirectory(cspCacheDir);
                  }
             } catch (Exception e) {
@@ -851,7 +850,6 @@ public class ModelSettingFragment extends BaseLazyFragment {
             }
         }).start();
         Toast.makeText(getContext(), "播放&JAR缓存已清空", Toast.LENGTH_LONG).show();
-        return;
     }
 
     private void initView() {
