741e320a5dac8abc9f7e3446533668d8cb60088d
daji888 <97334834+daji888@users.noreply.github.com>
2025-03-30 17:54:13 +0800

    Update BackupDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/BackupDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/BackupDialog.java
@@ -2,8 +2,10 @@ package com.github.tvbox.osc.ui.dialog;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Environment;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
@@ -146,7 +148,13 @@ public class BackupDialog extends BaseDialog {
                                 sharedPreferences.edit().putString(key, value).commit();
                             }
                         }
-                        Toast.makeText(getContext(), "恢复成功,请重启应用!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "恢复成功,即将自动重启应用!", Toast.LENGTH_SHORT).show();
+                        new Handler().postDelayed(new Runnable() {
+                             @Override
+                             public void run() {
+                                 restartApp();
+                             }
+                         }, 3000);
                     } else {
                         Toast.makeText(getContext(), "Hawk恢复失败!", Toast.LENGTH_SHORT).show();
                     }
@@ -158,6 +166,18 @@ public class BackupDialog extends BaseDialog {
             e.printStackTrace();
         }
     }
+
+    private void restartApp() {
+         Context context = getContext();
+         if (context != null) {
+             Intent i = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+             if (i != null) {
+                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                 context.startActivity(i);
+                 System.exit(0);
+             }
+         }
+     }
 
     void backup() {
         try {
