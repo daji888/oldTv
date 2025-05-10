9fd354af0f98c6db9fd9d471992c98335d8df9e4
daji888 <97334834+daji888@users.noreply.github.com>
2025-05-10 16:19:49 +0800

    Update JsLoader.java

--- app/src/main/java/com/github/catvod/crawler/JsLoader.java
+++ app/src/main/java/com/github/catvod/crawler/JsLoader.java
@@ -1,8 +1,10 @@
 package com.github.catvod.crawler;
 
+import android.util.Log;
 
 import com.github.tvbox.osc.base.App;
 
+import com.github.tvbox.osc.util.FileUtils;
 import com.github.tvbox.osc.util.LOG;
 import com.github.tvbox.osc.util.MD5;
 
@@ -21,16 +23,20 @@ import dalvik.system.DexClassLoader;
 import okhttp3.Response;
 
 public class JsLoader {
-    private static ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
-    private static ConcurrentHashMap<String, Class<?>> classs = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Class<?>> classes = new ConcurrentHashMap<>();
+    //当前的Js爬虫key
+    private volatile String recentKey = "";
 
-    public static void load() {
+    public static void destroy() {
         for (Spider spider : spiders.values()){
             spider.cancelByTag();
             spider.destroy();
         }
+    }
+    public void clear() {     
         spiders.clear();
-        classs.clear();
+        classes.clear();
     }
 
     public static void stopAll() {
@@ -53,7 +59,7 @@ public class JsLoader {
                 try {
                     classInit = classLoader.loadClass("com.github.catvod.js.Method");
                     if (classInit != null) {
-                        System.out.println("自定义jsapi加载成功!");
+                        Log.i("JSLoader", "echo-自定义jsapi代码加载成功!");
                         success = true;
                         break;
                     }
@@ -65,7 +71,7 @@ public class JsLoader {
             } while (count < 5);
 
             if (success) {
-                classs.put(key, classInit);
+                classes.put(key, classInit);
             }
         } catch (Throwable th) {
             th.printStackTrace();
@@ -74,13 +80,21 @@ public class JsLoader {
     }
 
     private Class<?> loadJarInternal(String jar, String md5, String key) {
-        if (classs.contains(key))
-            return classs.get(key);
-        File cache = new File(App.getInstance().getFilesDir().getAbsolutePath() + "/" + key + ".jar");
+        if (classes.containsKey(key)) {
+            Log.i("JSLoader", "echo-loadJarInternal cached");
+            return classes.get(key);
+        }
+        File cache = new File(App.getInstance().getFilesDir().getAbsolutePath() + "/csp/" + key + ".jar");
         if (!md5.isEmpty()) {
             if (cache.exists() && MD5.getFileMd5(cache).equalsIgnoreCase(md5)) {
                 loadClassLoader(cache.getAbsolutePath(), key);
-                return classs.get(key);
+                return classes.get(key);
+            }
+        } else {
+            if (cache.exists() && !FileUtils.isWeekAgo(cache)) {
+                if (loadClassLoader(cache.getAbsolutePath(), key)) {
+                    return classes.get(key);
+                }
             }
         }
         try {
@@ -102,16 +116,18 @@ public class JsLoader {
                 }
             }
             loadClassLoader(cache.getAbsolutePath(), key);
-            return classs.get(key);
+            return classes.get(key);
         } catch (Throwable e) {
             e.printStackTrace();
         }
         return null;
     }
-    private volatile String recentJarKey = "";
-
 
     public Spider getSpider(String key, String api, String ext, String jar) {
+        if (spiders.containsKey(key)) {
+            Log.i("JSLoader", "echo-getSpider cached");
+            return spiders.get(key);
+        }
         Class<?> classLoader = null;
         if (!jar.isEmpty()) {
             String[] urls = jar.split(";md5;");
@@ -120,29 +136,26 @@ public class JsLoader {
             String jarMd5 = urls.length > 1 ? urls[1].trim() : "";
             classLoader = loadJarInternal(jarUrl, jarMd5, jarKey);
         }
-        recentJarKey = key;
-        if (spiders.containsKey(key))
-            return spiders.get(key);
+        recentKey = key;
         try {
+            Log.i("JSLoader", "echo-getSpider load");
             Spider sp = new JsSpider(key, api, classLoader);
             sp.init(App.getInstance(), ext);
             spiders.put(key, sp);
             return sp;
         } catch (Throwable th) {
-            th.printStackTrace();
-            LOG.e("QuJS", th);
+            LOG.i("echo-getSpider-error "+th.getMessage());
         }
         return new SpiderNull();
     }
 
     public Object[] proxyInvoke(Map<String, String> params) {
         try {
-            Spider proxyFun = spiders.get(recentJarKey);
+            Spider proxyFun = spiders.get(recentKey);
             if (proxyFun != null) {
                 return proxyFun.proxyLocal(params);
             }
         } catch (Throwable th) {
-            LOG.e("proxyInvoke", th);
         }
         return null;
     }
