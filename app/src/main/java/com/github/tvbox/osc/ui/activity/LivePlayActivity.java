12112464036e05df310c195b15f8019740cafce6
daji888 <97334834+daji888@users.noreply.github.com>
2025-05-25 18:25:23 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -816,16 +816,16 @@ public class LivePlayActivity extends BaseActivity {
         }
     };
 
-    private void initLiveObj() {
+ /*   private void initLiveObj() {
         int position  =Hawk.get(HawkConfig.LIVE_GROUP_INDEX, 0);
-        JsonArray live_groups = Hawk.get(HawkConfig.LIVE_GROUP_LIST, 0);
+        JsonArray live_groups = Hawk.get(HawkConfig.LIVE_GROUP_LIST, new JsonArray());
         JsonObject livesOBJ = live_groups.get(position).getAsJsonObject();
         String type = livesOBJ.has("type") ? livesOBJ.get("type").getAsString() : "0";
         if (type.equals("3")) {
             String jarUrl = livesOBJ.has("jar") ? livesOBJ.get("jar").getAsString() : "";
             ApiConfig.get().setLiveJar(jarUrl);
         }
-    }
+    }  */
 
     private boolean playChannel(int channelGroupIndex, int liveChannelIndex, boolean changeSource) {
         if ((channelGroupIndex == currentChannelGroupIndex && liveChannelIndex == currentLiveChannelIndex && !changeSource)
@@ -1813,7 +1813,7 @@ public class LivePlayActivity extends BaseActivity {
             finish();
             return;
         }
-        initLiveObj(); 
+    //    initLiveObj(); 
         if (list.size() == 1 && list.get(0).getGroupName().startsWith("http://127.0.0.1")) {
             loadProxyLives(list.get(0).getGroupName());
         }
