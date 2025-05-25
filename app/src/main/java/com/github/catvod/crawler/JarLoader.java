be0c278bc27bb9f00367b981e6e376722de08baf
daji888 <97334834+daji888@users.noreply.github.com>
2025-05-25 16:29:09 +0800

    Update JarLoader.java

--- app/src/main/java/com/github/catvod/crawler/JarLoader.java
+++ app/src/main/java/com/github/catvod/crawler/JarLoader.java
@@ -38,6 +38,18 @@ public class JarLoader {
         recentJarKey = "main";
         return loadClassLoader(cache, recentJarKey);
     }
+
+    public void setRecentJarKey(String key) {
+        recentJarKey = key;
+    }
+
+    public void loadLiveJar(String jarUrl) {
+        String[] urls = jarUrl.split(";md5;");
+        jarUrl = urls[0];
+        String jarKey = MD5.string2MD5(jarUrl);
+        String jarMd5 = urls.length > 1 ? urls[1].trim() : "";
+        loadJarInternal(jarUrl,jarMd5,jarKey);
+    }
  
     public void clear() {
         spiders.clear();
