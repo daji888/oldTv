29040c6b063ca94dd669ced5020f6253f9677e9c
daji888 <97334834+daji888@users.noreply.github.com>
2025-05-26 16:43:57 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1813,7 +1813,7 @@ public class LivePlayActivity extends BaseActivity {
             finish();
             return;
         }
-        initLiveObj(); 
+     //   initLiveObj(); 
         if (list.size() == 1 && list.get(0).getGroupName().startsWith("http://127.0.0.1")) {
             loadProxyLives(list.get(0).getGroupName());
         }
