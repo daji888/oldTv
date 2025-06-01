0b11660a113b8c4adcb6568e641b6f6aacb81fd4
daji888 <97334834+daji888@users.noreply.github.com>
2025-06-01 11:06:32 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -51,6 +51,7 @@ public class HawkConfig {
 //    public static final String LIVE_PLAYER_TYPE = "live_player_type"; // 0 系统 1 ijk 2 exo
     public static final String LIVE_PLAY_TYPE = "live_play_type";//0 系统 1 ijk 2 exo 10 MXPlayer
     public static final String PLAYER_IS_LIVE = "player_is_live";
+    public static final String LIVE_WEB_HEADER = "live_web_header";
     public static final String DOH_JSON = "doh_json";
     
      public static boolean isDebug() {
