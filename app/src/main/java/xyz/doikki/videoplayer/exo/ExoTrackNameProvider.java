ac36c9896e2601f1caad8749ead5f2fcf46aa72e
daji888 <97334834+daji888@users.noreply.github.com>
2025-08-14 14:19:54 +0800

    Update ExoTrackNameProvider.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
@@ -33,7 +33,7 @@ public class ExoTrackNameProvider {
         if (trackType == C.TRACK_TYPE_VIDEO) {
             trackName =
                     joinWithSeparator(
-                            buildMimeString(format),
+                            buildMimeString(trackType, format),
                             buildRoleString(format), 
                             buildResolutionString(format), 
                             buildBitrateString(format), 
@@ -43,23 +43,21 @@ public class ExoTrackNameProvider {
                     joinWithSeparator(
                             buildLanguageOrLabelString(format),
                             buildAudioChannelString(format),
-                            buildMimeString(format), 
+                            buildMimeString(trackType, format), 
                             buildBitrateString(format));
-        } else if (trackType == C.TRACK_TYPE_TEXT) {
+        } else {
             trackName =
                     joinWithSeparator(
                             buildLanguageOrLabelString(format),
-                            buildMimeString(format));     
-        } else {
-            trackName = buildLanguageOrLabelString(format);
+                            buildMimeString(trackType, format));
         }
-        if (trackName.length() != 0) {
-      return trackName;
-    }
-    @Nullable String language = format.language;
-    return (language == null || language.trim().isEmpty())
-        ? resources.getString(R.string.exo_track_unknown)
-        : resources.getString(R.string.exo_track_unknown_name, language);
+        if (!trackName.isEmpty()) {
+            return trackName;
+        }
+        @Nullable String language = format.language;
+        return (language == null || language.trim().isEmpty())
+            ? resources.getString(R.string.exo_track_unknown)
+            : resources.getString(R.string.exo_track_unknown_name, language);
    }
 
     private String buildResolutionString(Format format) {
@@ -117,8 +115,7 @@ public class ExoTrackNameProvider {
       if (TextUtils.isEmpty(language) || C.LANGUAGE_UNDETERMINED.equals(language)) {
         return "";
       }
-      Locale languageLocale =
-          Util.SDK_INT >= 21 ? Locale.forLanguageTag(language) : new Locale(language);
+      Locale languageLocale = Locale.forLanguageTag(language);
       Locale displayLocale = Util.getDefaultDisplayLocale();
       String languageName = languageLocale.getDisplayName(displayLocale);
       if (TextUtils.isEmpty(languageName)) {
@@ -156,7 +153,7 @@ public class ExoTrackNameProvider {
     private String joinWithSeparator(String... items) {
       String itemList = "";
       for (String item : items) {
-        if (item.length() > 0) {
+        if (!item.isEmpty()) {
           if (TextUtils.isEmpty(itemList)) {
             itemList = item;
           } else {
@@ -190,97 +187,56 @@ public class ExoTrackNameProvider {
         return C.TRACK_TYPE_UNKNOWN;
     }
 
-    private String buildMimeString(Format format) {
-        if (format.sampleMimeType == null) return "";
-        return buildMimeString(format.sampleMimeType);
+    private String buildMimeString(int trackType, Format format) {
+        if (trackType == C.TRACK_TYPE_TEXT && format.codecs != null) return buildMimeString(format.codecs);
+        if (format.sampleMimeType != null) return buildMimeString(format.sampleMimeType);
+        return "";
     }
 
     private String buildMimeString(String mimeType) {
-        switch (mimeType) {
-            case MimeTypes.AUDIO_DTS:
-                return "DTS";
-            case MimeTypes.AUDIO_DTS_HD:
-                return "DTS-HD";
-            case MimeTypes.AUDIO_DTS_EXPRESS:
-                return "DTS Express";
-            case MimeTypes.AUDIO_TRUEHD:
-                return "TrueHD";
-            case MimeTypes.AUDIO_AC3:
-                return "AC-3";
-            case MimeTypes.AUDIO_E_AC3:
-                return "E-AC-3";
-            case MimeTypes.AUDIO_E_AC3_JOC:
-                return "E-AC-3-JOC";
-            case MimeTypes.AUDIO_AC4:
-                return "AC-4";
-            case MimeTypes.AUDIO_AAC:
-                return "AAC";
-            case MimeTypes.AUDIO_MPEG:
-                return "MP3";
-            case MimeTypes.AUDIO_MPEG_L2:
-                return "MP2";
-            case MimeTypes.AUDIO_VORBIS:
-                return "Vorbis";
-            case MimeTypes.AUDIO_OPUS:
-                return "Opus";
-            case MimeTypes.AUDIO_FLAC:
-                return "FLAC";
-            case MimeTypes.AUDIO_ALAC:
-                return "ALAC";
-            case MimeTypes.AUDIO_WAV:
-                return "WAV";
-            case MimeTypes.AUDIO_AMR:
-                return "AMR";
-            case MimeTypes.AUDIO_AMR_NB:
-                return "AMR-NB";
-            case MimeTypes.AUDIO_AMR_WB:
-                return "AMR-WB";
-            case MimeTypes.VIDEO_MP4:
-                return "MP4";
-            case MimeTypes.VIDEO_FLV:
-                return "FLV";
-            case MimeTypes.VIDEO_AV1:
-                return "AV1";
-            case MimeTypes.VIDEO_AVI:
-                return "AVI";
-            case MimeTypes.VIDEO_MPEG:
-                return "MPEG";
-            case MimeTypes.VIDEO_MPEG2:
-                return "MPEG2";
-            case MimeTypes.VIDEO_H263:
-                return "H263";
-            case MimeTypes.VIDEO_H264:
-                return "H264";
-            case MimeTypes.VIDEO_H265:
-                return "H265";
-            case MimeTypes.VIDEO_VP8:
-                return "VP8";
-            case MimeTypes.VIDEO_VP9:
-                return "VP9";
-            case MimeTypes.VIDEO_DOLBY_VISION:
-                return "DOLBY";
-            case MimeTypes.TEXT_SSA:
-                return "SSA";
-            case MimeTypes.TEXT_VTT:
-                return "VTT";
-            case MimeTypes.APPLICATION_PGS:
-                return "PGS";
-            case MimeTypes.APPLICATION_SUBRIP:
-                return "SRT";    
-            case MimeTypes.APPLICATION_TTML:
-                return "TTML";
-            case MimeTypes.APPLICATION_TX3G:
-                return "TX3G";
-            case MimeTypes.APPLICATION_DVBSUBS:
-                return "DVB";
-            case MimeTypes.APPLICATION_MEDIA3_CUES:
-                return "CUES";
-            case MimeTypes.APPLICATION_CEA608:
-                return "CEA-608";
-            case MimeTypes.APPLICATION_CEA708:
-                return "CEA-708";    
-            default:
-                return mimeType;
-        }
+        if (mimeType.contains(MimeTypes.AUDIO_DTS)) return "DTS";
+        else if (mimeType.contains(MimeTypes.AUDIO_DTS_HD)) return "DTS-HD";
+        else if (mimeType.contains(MimeTypes.AUDIO_DTS_EXPRESS)) return "DTS Express";
+        else if (mimeType.contains(MimeTypes.AUDIO_TRUEHD)) return "TrueHD";
+        else if (mimeType.contains(MimeTypes.AUDIO_AC3)) return "AC-3";
+        else if (mimeType.contains(MimeTypes.AUDIO_E_AC3)) return "E-AC-3";
+        else if (mimeType.contains(MimeTypes.AUDIO_E_AC3_JOC)) return "E-AC-3-JOC";
+        else if (mimeType.contains(MimeTypes.AUDIO_AC4)) return "AC-4";
+        else if (mimeType.contains(MimeTypes.AUDIO_AAC)) return "AAC";
+        else if (mimeType.contains(MimeTypes.AUDIO_MPEG)) return "MP3";
+        else if (mimeType.contains(MimeTypes.AUDIO_MPEG_L2)) return "MP2";
+        else if (mimeType.contains(MimeTypes.AUDIO_VORBIS)) return "Vorbis";
+        else if (mimeType.contains(MimeTypes.AUDIO_OPUS)) return "Opus";
+        else if (mimeType.contains(MimeTypes.AUDIO_FLAC)) return "FLAC";
+        else if (mimeType.contains(MimeTypes.AUDIO_ALAC)) return "ALAC";
+        else if (mimeType.contains(MimeTypes.AUDIO_WAV)) return "WAV";
+        else if (mimeType.contains(MimeTypes.AUDIO_AMR)) return "AMR";
+        else if (mimeType.contains(MimeTypes.AUDIO_AMR_NB)) return "AMR-NB";
+        else if (mimeType.contains(MimeTypes.AUDIO_AMR_WB)) return "AMR-WB";
+        else if (mimeType.contains(MimeTypes.VIDEO_MP4)) return "MP4";
+        else if (mimeType.contains(MimeTypes.VIDEO_FLV)) return "FLV";
+        else if (mimeType.contains(MimeTypes.VIDEO_AV1)) return "AV1";
+        else if (mimeType.contains(MimeTypes.VIDEO_AVI)) return "AVI";
+        else if (mimeType.contains(MimeTypes.VIDEO_MPEG)) return "MPEG";
+        else if (mimeType.contains(MimeTypes.VIDEO_MPEG2)) return "MPEG2";
+        else if (mimeType.contains(MimeTypes.VIDEO_H263)) return "H263";
+        else if (mimeType.contains(MimeTypes.VIDEO_H264)) return "H264";
+        else if (mimeType.contains(MimeTypes.VIDEO_H265)) return "H265";
+        else if (mimeType.contains(MimeTypes.VIDEO_VC1)) return "VC1";
+        else if (mimeType.contains(MimeTypes.VIDEO_VP8)) return "VP8";
+        else if (mimeType.contains(MimeTypes.VIDEO_VP9)) return "VP9";
+        else if (mimeType.contains(MimeTypes.VIDEO_DIVX)) return "DIVX";
+        else if (mimeType.contains(MimeTypes.VIDEO_DOLBY_VISION)) return "DOLBY";
+        else if (mimeType.contains(MimeTypes.TEXT_SSA)) return "SSA";
+        else if (mimeType.contains(MimeTypes.TEXT_VTT)) return "VTT";
+        else if (mimeType.contains(MimeTypes.APPLICATION_PGS)) return "PGS";
+        else if (mimeType.contains(MimeTypes.APPLICATION_SUBRIP)) return "SRT";
+        else if (mimeType.contains(MimeTypes.APPLICATION_TTML)) return "TTML";
+        else if (mimeType.contains(MimeTypes.APPLICATION_TX3G)) return "TX3G";
+        else if (mimeType.contains(MimeTypes.APPLICATION_DVBSUBS)) return "DVB";
+        else if (mimeType.contains(MimeTypes.APPLICATION_MEDIA3_CUES)) return "CUES";
+        else if (mimeType.contains(MimeTypes.APPLICATION_CEA608)) return "CEA-608";
+        else if (mimeType.contains(MimeTypes.APPLICATION_CEA708)) return "CEA-708";    
+        else return mimeType;
     }
 }
