2b21e738305f4a2da368a3eecd45d94e3ce2fa19
daji888 <97334834+daji888@users.noreply.github.com>
2025-08-15 18:00:42 +0800

    Update IjkTrackInfo.java

--- app/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
+++ app/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
@@ -84,18 +84,18 @@ public class IjkTrackInfo implements ITrackInfo {
                 out.append(mStreamMeta.getSampleRateInline());
                 out.append("，");
                 out.append(mStreamMeta.getBitrateInline());
-            //    out.append("，");
-            //    out.append(getLanguage());
+                out.append("，");
+                out.append(getLanguage());
                 break;
             case MEDIA_TRACK_TYPE_TIMEDTEXT:
                 out.append("TIMEDTEXT");
-            //    out.append("，");
-            //    out.append(getLanguage());
+                out.append("，");
+                out.append(getLanguage());
                 break;
             case MEDIA_TRACK_TYPE_SUBTITLE:
                 out.append("SUBTITLE");
-            //    out.append("，");
-            //    out.append(getLanguage());
+                out.append("，");
+                out.append(getLanguage());
                 break;
             case MEDIA_TRACK_TYPE_METADATA:
                 out.append("METADATA");
