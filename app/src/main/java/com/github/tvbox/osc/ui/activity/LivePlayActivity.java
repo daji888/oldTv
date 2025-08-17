7a360ee575f12add2ef62c8bd8e5617742eb2f2a
daji888 <97334834+daji888@users.noreply.github.com>
2025-08-17 19:52:07 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2196,6 +2196,9 @@ public class LivePlayActivity extends BaseActivity {
             int pos = Hawk.get(HawkConfig.LIVE_SOURCE, -1);
             if (pos > -1) {
                 currentLiveChannelItem.setSourceIndex(Hawk.get(HawkConfig.LIVE_SOURCE));
+                if (pos > currentLiveChannelItem.getSourceNum() - 1) {
+                    currentLiveChannelItem.setSourceIndex(0);
+                }    
                 playChannel(groupIndex, liveChannelIndex, true);
             } else {
                 playChannel(groupIndex, liveChannelIndex, false);
