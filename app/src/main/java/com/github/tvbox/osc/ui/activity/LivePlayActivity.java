dd2fbe1ae50b1beca6a66da28a160a2b7ef276fe
daji888 <97334834+daji888@users.noreply.github.com>
2025-08-29 20:25:02 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -814,9 +814,9 @@ public class LivePlayActivity extends BaseActivity {
         if (!changeSource) {
             currentChannelGroupIndex = channelGroupIndex;
             currentLiveChannelIndex = liveChannelIndex;
-            String currentLiveChannelGroup = liveChannelGroupList.get(currentLiveChannelIndex);
+         //   currentLiveChannelGroup = liveChannelGroupList.get(currentLiveChannelIndex);
             currentLiveChannelItem = getLiveChannels(currentChannelGroupIndex).get(currentLiveChannelIndex);
-            Hawk.put(HawkConfig.LIVE_GROUP, currentLiveChannelGroup.getGroupName());
+            Hawk.put(HawkConfig.LIVE_GROUP, liveChannelGroupList.get(currentLiveChannelIndex).getGroupName());
             Hawk.put(HawkConfig.LIVE_CHANNEL, currentLiveChannelItem.getChannelName());
             livePlayerManager.getLiveChannelPlayer(mVideoView, currentLiveChannelItem.getChannelName());
         } else {
