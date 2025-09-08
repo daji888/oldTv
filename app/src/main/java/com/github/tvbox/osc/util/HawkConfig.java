daea54013193107f019dd3ccdadd0f8fccc5201e
daji888 <97334834+daji888@users.noreply.github.com>
2025-09-08 15:59:28 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -29,6 +29,7 @@ public class HawkConfig {
     public static final String HOME_REC = "home_rec"; // 0 豆瓣热播 1 数据源推荐 2 观看历史
     public static final String HISTORY_NUM = "history_num";
     public static final String SEARCH_VIEW = "search_view"; // 0 列表 1 缩略图
+    public static final String LIVE_GROUP = "last_live_group_name";
     public static final String LIVE_CHANNEL = "last_live_channel_name";
     public static final String LIVE_SOURCE = "last_live_source_index";
     public static final String LIVE_CHANNEL_REVERSE = "live_channel_reverse";
