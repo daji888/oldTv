5fbe35d741da5f9c47dbf0c2db5baeb570962758
daji888 <97334834+daji888@users.noreply.github.com>
2025-09-25 19:53:47 +0800

    Update DefaultConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
@@ -185,8 +185,8 @@ public class DefaultConfig {
     }
 
     private static final Pattern snifferMatch = Pattern.compile(
-            "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)\\?.*|" +
-            "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)|" +
+            "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|mp3|rm|wmv|mpg|m4a|aac|mpd)\\?.*|" +
+            "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|mp3|rm|wmv|mpg|m4a|aac|mpd)|" +
             "http((?!http).)*?video/tos*|" +
             "http((?!http).){20,}?/m3u8\\?pt=m3u8.*|" +
             "http((?!http).)*?default\\.ixigua\\.com/.*|" +
