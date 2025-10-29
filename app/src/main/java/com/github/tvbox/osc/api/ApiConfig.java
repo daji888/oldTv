6c4e08088a625dc83f1561248ac46b3c0151998c
daji888 <97334834+daji888@users.noreply.github.com>
2025-10-29 17:13:34 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -548,30 +548,6 @@ public class ApiConfig {
             }
         }
 
-        myHosts = new HashMap<>();
-        if (infoJson.has("hosts")) {
-            JsonArray hostsArray = infoJson.getAsJsonArray("hosts");
-            for (int i = 0; i < hostsArray.size(); i++) {
-                String entry = hostsArray.get(i).getAsString();
-                String[] parts = entry.split("=", 2); // 只分割一次，防止 value 里有 =
-                if (parts.length == 2) {
-                    myHosts.put(parts[0], parts[1]);
-                }
-            }
-        }
-
-        if (infoJson.has("doh")) {
-            String doh_json = infoJson.getAsJsonArray("doh").toString();
-            if (!Hawk.get(HawkConfig.DOH_JSON, "").equals(doh_json)) {
-                 Hawk.put(HawkConfig.DOH_URL, 0);
-                 Hawk.put(HawkConfig.DOH_JSON, doh_json);
-             }
-        } else {
-            Hawk.put(HawkConfig.DOH_JSON, "");
-        }
-        OkGoHelper.setDnsList();
-        LOG.i("echo-api-config-----------load");
-
         String defaultIJKADS = "{\"ijk\":[{\"options\":[{\"name\":\"mediacodec\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-all-videos\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-auto-rotate\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-handle-resolution-change\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-avc\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-hevc\",\"category\":4,\"value\":\"1\"}],\"group\":\"硬解\"},{\"options\":[{\"name\":\"mediacodec\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-all-videos\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-auto-rotate\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-handle-resolution-change\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-avc\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-hevc\",\"category\":4,\"value\":\"0\"}],\"group\":\"软解\"}],\"ads\":[\"mimg.0c1q0l.cn\",\"www.googletagmanager.com\",\"www.google-analytics.com\",\"mc.usihnbcq.cn\",\"mg.g1mm3d.cn\",\"mscs.svaeuzh.cn\",\"cnzz.hhttm.top\",\"tp.vinuxhome.com\",\"cnzz.mmstat.com\",\"www.baihuillq.com\",\"s23.cnzz.com\",\"z3.cnzz.com\",\"c.cnzz.com\",\"stj.v1vo.top\",\"z12.cnzz.com\",\"img.mosflower.cn\",\"tips.gamevvip.com\",\"ehwe.yhdtns.com\",\"xdn.cqqc3.com\",\"www.jixunkyy.cn\",\"sp.chemacid.cn\",\"hm.baidu.com\",\"s9.cnzz.com\",\"z6.cnzz.com\",\"um.cavuc.com\",\"mav.mavuz.com\",\"wofwk.aoidf3.com\",\"z5.cnzz.com\",\"xc.hubeijieshikj.cn\",\"tj.tianwenhu.com\",\"xg.gars57.cn\",\"k.jinxiuzhilv.com\",\"cdn.bootcss.com\",\"ppl.xunzhuo123.com\",\"xomk.jiangjunmh.top\",\"img.xunzhuo123.com\",\"z1.cnzz.com\",\"s13.cnzz.com\",\"xg.huataisangao.cn\",\"z7.cnzz.com\",\"xg.huataisangao.cn\",\"z2.cnzz.com\",\"s96.cnzz.com\",\"q11.cnzz.com\",\"thy.dacedsfa.cn\",\"xg.whsbpw.cn\",\"s19.cnzz.com\",\"z8.cnzz.com\",\"s4.cnzz.com\",\"f5w.as12df.top\",\"ae01.alicdn.com\",\"www.92424.cn\",\"k.wudejia.com\",\"vivovip.mmszxc.top\",\"qiu.xixiqiu.com\",\"cdnjs.hnfenxun.com\",\"cms.qdwght.com\"]}";
         String defaultEXO = "{\"exo\":[{\"options\":[{\"extensionRendererMode\":\"EXTENSION_RENDERER_MODE_OFF\"}],\"group\":\"硬解\"},{\"options\":[{\"extensionRendererMode\":\"EXTENSION_RENDERER_MODE_ON\"}],\"group\":\"硬软\"},{\"options\":[{\"extensionRendererMode\":\"EXTENSION_RENDERER_MODE_PREFER\"}],\"group\":\"软硬\"}]}";
         JsonObject defaultJson = new Gson().fromJson(defaultIJKADS, JsonObject.class);
