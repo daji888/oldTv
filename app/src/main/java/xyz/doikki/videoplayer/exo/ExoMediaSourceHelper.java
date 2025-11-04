4d66c6fed63b5fb6bc5eb8ad73d8cf38b600a7f3
daji888 <97334834+daji888@users.noreply.github.com>
2025-11-04 16:59:08 +0800

    Update ExoMediaSourceHelper.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -21,6 +21,7 @@ import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor;
 import androidx.media3.datasource.cache.SimpleCache;
 import androidx.media3.datasource.rtmp.RtmpDataSource;
 import androidx.media3.exoplayer.dash.DashMediaSource;
+import androidx.media3.exoplayer.hls.DefaultHlsExtractorFactory;
 import androidx.media3.exoplayer.hls.HlsMediaSource;
 import androidx.media3.exoplayer.rtsp.RtspMediaSource;
 import androidx.media3.exoplayer.smoothstreaming.SsMediaSource;
@@ -130,7 +131,11 @@ public final class ExoMediaSourceHelper {
             case C.CONTENT_TYPE_DASH:
                 return new DashMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
             case C.CONTENT_TYPE_HLS:
-                return new HlsMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
+            //    return new HlsMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
+                return new HlsMediaSource.Factory(mHttpDataSourceFactory)
+                        .setAllowChunklessPreparation(true)
+                        .setExtractorFactory(new DefaultHlsExtractorFactory())
+                        .createMediaSource(MediaItem.fromUri(contentUri));
             case C.CONTENT_TYPE_SS:
                 return new SsMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));    
             default:
