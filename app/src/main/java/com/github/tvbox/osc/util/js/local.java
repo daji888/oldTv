1a610588b3f8bad9bbcba9df8ee06e4b63119705
daji888 <97334834+daji888@users.noreply.github.com>
2025-11-27 17:23:39 +0800

    Update local.java

--- app/src/main/java/com/github/tvbox/osc/util/js/local.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/local.java
@@ -2,16 +2,16 @@ package com.github.tvbox.osc.util.js;
 
 import androidx.annotation.Keep;
 import com.orhanobut.hawk.Hawk;
-import com.whl.quickjs.wrapper.Function;
+import com.whl.quickjs.wrapper.JSMethod;
 
-public class local {@Keep@Function
+public class local {@Keep@JSMethod
     public void delete(String str, String str2) {
         try {
             Hawk.delete("jsRuntime_" + str + "_" + str2);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }@Keep@Function
+    }@Keep@JSMethod
     public String get(String str, String str2) {
         try {
             return Hawk.get("jsRuntime_" + str + "_" + str2, "");
@@ -19,7 +19,7 @@ public class local {@Keep@Function
             Hawk.delete(str);
             return str2;
         }
-    }@Keep@Function
+    }@Keep@JSMethod
     public void set(String str, String str2, String str3) {
         try {
             Hawk.put("jsRuntime_" + str + "_" + str2, str3);
