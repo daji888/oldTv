67b595e0c8b0a34a77e319f5e0559246bcb8c930
daji888 <97334834+daji888@users.noreply.github.com>
2025-12-04 16:22:44 +0800

    Update JsSpider.java

--- app/src/main/java/com/github/tvbox/osc/util/js/JsSpider.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/JsSpider.java
@@ -217,7 +217,6 @@ public class JsSpider extends Spider {
             if (content.startsWith("//bb")) {
                 cat = true;
                 byte[] b = Base64.decode(content.replace("//bb",""), 0);
-  //              ctx.execute(byteFF(b), key + ".js");
                 ctx.execute(byteFF(b));
                 ctx.evaluateModule(String.format(SPIDER_STRING_CODE, key + ".js") + "globalThis." + key + " = __JS_SPIDER__;", "tv_box_root.js");
                 //ctx.execute(byteFF(b), key + ".js","__jsEvalReturn");
