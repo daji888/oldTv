e7acfabf4a2036fb6cc5374c59e62d59f6c9c0ea
daji888 <97334834+daji888@users.noreply.github.com>
2025-12-25 16:03:10 +0800

    Update SelectDialogAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
@@ -20,10 +20,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.SelectViewHolder> {
+public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.SelectViewHolder<T>> {
 
-    class SelectViewHolder extends RecyclerView.ViewHolder {
-
+    static class SelectViewHolder<T> extends RecyclerView.ViewHolder {
         public SelectViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
         }
@@ -31,13 +30,10 @@ public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.S
 
     public interface SelectDialogInterface<T> {
         void click(T value, int pos);
-
         String getDisplay(T val);
     }
 
-
     public static DiffUtil.ItemCallback<String> stringDiff = new DiffUtil.ItemCallback<String>() {
-
         @Override
         public boolean areItemsTheSame(@NonNull @NotNull String oldItem, @NonNull @NotNull String newItem) {
             return oldItem.equals(newItem);
@@ -49,14 +45,11 @@ public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.S
         }
     };
 
-
     private ArrayList<T> data = new ArrayList<>();
-
     private int select = 0;
+    private SelectDialogInterface<T> dialogInterface;
 
-    private SelectDialogInterface dialogInterface;
-
-    public SelectDialogAdapter(SelectDialogInterface dialogInterface, DiffUtil.ItemCallback diffCallback) {
+    public SelectDialogAdapter(SelectDialogInterface<T> dialogInterface, DiffUtil.ItemCallback<T> diffCallback) {
         super(diffCallback);
         this.dialogInterface = dialogInterface;
     }
@@ -73,23 +66,22 @@ public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.S
         return data.size();
     }
 
-
     @Override
-    public SelectDialogAdapter.SelectViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return new SelectDialogAdapter.SelectViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dialog_select, parent, false));
+    public SelectViewHolder<T> onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return new SelectViewHolder<>(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dialog_select, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @NotNull SelectDialogAdapter.SelectViewHolder holder, @SuppressLint("RecyclerView") int position) {    
+    public void onBindViewHolder(@NonNull @NotNull SelectViewHolder<T> holder, @SuppressLint("RecyclerView") int position) {    
         T value = data.get(position);
         String name = dialogInterface.getDisplay(value);
         TextView view = holder.itemView.findViewById(R.id.tvName);
         if (position == select) {
             view.setTextColor(0xffFF9900);
-            view .setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            view.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         } else {
             view.setTextColor(0xD9FFFFFF);
-            view .setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+            view.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
         }
         view.setText(name);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
