b95e6af43fd808ca51e42dbd7c384bcbfe5bc809
daji888 <97334834+daji888@users.noreply.github.com>
2026-01-08 15:49:17 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -758,7 +758,7 @@ public class PlayFragment extends BaseLazyFragment {
                                 startPlayUrl(url, headers);
                             else {
                                 startPlayUrl("http://127.0.0.1:" + RemoteServer.serverPort + "/m3u8", headers);
-                                //Toast.makeText(getContext(), "已移除视频广告", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getContext(), "已移除视频广告 " + M3U8.currentAdCount + " 条", Toast.LENGTH_SHORT).show();
                             }
                             return;
                         }
@@ -777,7 +777,7 @@ public class PlayFragment extends BaseLazyFragment {
                                             startPlayUrl(finalforwardurl, headers);
                                         else {
                                             startPlayUrl("http://127.0.0.1:" + RemoteServer.serverPort + "/m3u8", headers);
-                                            //Toast.makeText(getContext(), "已移除视频广告", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(getContext(), "已移除视频广告 " + M3U8.currentAdCount + " 条", Toast.LENGTH_SHORT).show();
                                         }
                                     }
 
