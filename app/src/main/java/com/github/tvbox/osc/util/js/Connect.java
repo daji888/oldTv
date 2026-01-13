5dc9197c7ccad46a749d6d230b7ed8ed98b993a0
daji888 <97334834+daji888@users.noreply.github.com>
2026-01-13 17:32:06 +0800

    Update Connect.java

--- app/src/main/java/com/github/tvbox/osc/util/js/Connect.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/Connect.java
@@ -39,6 +39,7 @@ public class Connect {
             JSObject jsObject = ctx.createJSObject();
             JSObject jsHeader = ctx.createJSObject();
             setHeader(ctx, res, jsHeader);
+            jsObject.set("code", res.code());
             jsObject.set("headers", jsHeader);
             if (req.getBuffer() == 0) jsObject.set("content", new String(res.body().bytes(), req.getCharset()));
             if (req.getBuffer() == 1) {
@@ -47,6 +48,7 @@ public class Connect {
                 jsObject.set("content", array);
             }
             if (req.getBuffer() == 2) jsObject.set("content", Base64.encodeToString(res.body().bytes(), Base64.DEFAULT | Base64.NO_WRAP));
+            if (req.getBuffer() == 3) jsObject.set("content", res.body().bytes());
             return jsObject;
         } catch (Exception e) {
             return error(ctx);
@@ -58,6 +60,7 @@ public class Connect {
         JSObject jsHeader = ctx.createJSObject();
         jsObject.set("headers", jsHeader);
         jsObject.set("content", "");
+        jsObject.set("code", "");
         return jsObject;
     }
 
@@ -75,12 +78,12 @@ public class Connect {
         if (req.getData() != null && req.getPostType().equals("json")) return getJsonBody(req);
         if (req.getData() != null && req.getPostType().equals("form")) return getFormBody(req);
         if (req.getData() != null && req.getPostType().equals("form-data")) return getFormDataBody(req);
-        if (req.getBody() != null && contentType != null) return RequestBody.create(MediaType.get(contentType), req.getBody());
-        return RequestBody.create(null, "");
+        if (req.getBody() != null && contentType != null) return RequestBody.create(req.getBody(), MediaType.get(contentType));
+        return RequestBody.create(new byte[0]);
     }
 
     private static RequestBody getJsonBody(Req req) {
-        return RequestBody.create(MediaType.get("application/json; charset=utf-8"), req.getData().toString());
+        return RequestBody.create(req.getData().toString(), MediaType.get("application/json; charset=utf-8"));
     }
 
     private static RequestBody getFormBody(Req req) {
