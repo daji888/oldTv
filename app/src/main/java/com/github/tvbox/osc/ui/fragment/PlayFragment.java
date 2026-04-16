10e5fcfa91b9e30bfdcddb7885ed0be750406c9b
daji888 <97334834+daji888@users.noreply.github.com>
2026-04-16 17:06:45 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -299,8 +299,6 @@ public class PlayFragment extends BaseLazyFragment {
 
             @Override
             public void prepared() {
-                initVideoView();
-                initAudioView();
                 initSubtitleView();
             }
 
@@ -844,32 +842,6 @@ public class PlayFragment extends BaseLazyFragment {
             }
         });
     }
-
-    private void initVideoView() {
-        AbstractPlayer mediaPlayer = mVideoView.getMediaPlayer();
-        TrackInfo trackInfo = null;
-        if (mediaPlayer instanceof EXOmPlayer) {
-            //默认选中第一个视轨 一般第一个视轨分辨率最高
-            trackInfo = ((EXOmPlayer) mediaPlayer).getTrackInfo();
-            if (trackInfo != null && trackInfo.getVideo().size() > 1) {
-                TrackInfoBean firsVideo = trackInfo.getVideo().get(0);
-                ((EXOmPlayer) mediaPlayer).selectExoTrack(firsVideo);
-            }
-         }
-     }
-
-    private void initAudioView() {
-        AbstractPlayer mediaPlayer = mVideoView.getMediaPlayer();
-        TrackInfo trackInfo = null;
-        if (mediaPlayer instanceof IjkmPlayer) {
-            //默认选中第一个音轨 一般第一个音轨是国语
-            trackInfo = ((IjkmPlayer) mediaPlayer).getTrackInfo();
-            if (trackInfo != null && trackInfo.getAudio().size() > 1) {
-                int firsIndex = trackInfo.getAudio().get(0).trackId;
-                ((IjkmPlayer) mediaPlayer).setTrack(firsIndex);
-            }
-         }
-     }
     
     private void initSubtitleView() {
         AbstractPlayer mediaPlayer = mVideoView.getMediaPlayer();
