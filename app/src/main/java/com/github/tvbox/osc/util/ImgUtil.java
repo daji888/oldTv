5440d693361f275c76813dd30eb1b5eea34e9be7
daji888 <97334834+daji888@users.noreply.github.com>
2026-07-12 14:53:18 +0800

    Update ImgUtil.java

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -1,12 +1,14 @@
 package com.github.tvbox.osc.util;
 
 import static com.bumptech.glide.load.resource.bitmap.VideoDecoder.FRAME_OPTION;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
 import android.util.Base64;
 import android.text.TextUtils;
 import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
@@ -20,50 +22,19 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
-import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.base.App;
-import com.google.common.net.HttpHeaders;
+import com.github.tvbox.osc.R;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImgUtil {
-//    public static void load(String url, ImageView view) {
-//        load(url, view, 10);
-//    }
-//
-//    public static void load(String url, ImageView view, ImageView.ScaleType scaleType) {
-//        load(url, view, 10, scaleType);
-//    }
-//
-//    public static void load(String url, ImageView view, int roundingRadius, ImageView.ScaleType scaleType) {
-//        view.setScaleType(ImageView.ScaleType.FIT_CENTER);
-//        if (TextUtils.isEmpty(url)) {
-//            view.setImageResource(R.drawable.img_loading_placeholder);
-//        } else {
-//            if (roundingRadius == 0) roundingRadius = 1;
-//            RequestOptions requestOptions = new RequestOptions()
-//                .format(DecodeFormat.PREFER_RGB_565)
-//                .diskCacheStrategy(getDiskCacheStrategy(4))
-//                .dontAnimate()
-//                .transform(
-//            new RoundedCorners(roundingRadius));
-//            Glide.with(App.getInstance())
-//                .asBitmap()
-//                .load(getUrl(url))
-//                .error(R.drawable.img_loading_placeholder)
-//                .placeholder(R.drawable.img_loading_placeholder)
-//                .listener(getListener(view, scaleType))
-//                .apply(requestOptions)
-//                .into(view);
-//        }
-//    }
-
     public static boolean isBase64Image(String picUrl) {
-         return picUrl.startsWith("data:image");
+         return picUrl != null && picUrl.startsWith("data:image");
      }
  
      public static Bitmap decodeBase64ToBitmap(String base64Str) {
@@ -81,7 +52,7 @@ public class ImgUtil {
             if (roundingRadius == 0) roundingRadius = 1;
             RequestOptions requestOptions = new RequestOptions()
                 .format(DecodeFormat.PREFER_RGB_565)
-                .diskCacheStrategy(getDiskCacheStrategy(1))
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .dontAnimate()
                 .transform(new RoundedCorners(roundingRadius));
             Glide.with(App.getInstance())
@@ -112,48 +83,17 @@ public class ImgUtil {
             .into(imageView);
     }
 
-    public static String getDiskCacheStrategyName(int index) {
-        String[] names = new String[] {
-            "[NONE] 关闭", "[RESOURCE] 转换图片", "[DATA] 原始图片 ", "[ALL] 原始图片和转换图片", "[AUTOMATIC] 自动"
-        };
-        return names[index];
-    }
-
-    public static DiskCacheStrategy getDiskCacheStrategy(int index) {
-        switch (index) {
-            case 1:
-                return DiskCacheStrategy.RESOURCE;
-            case 2:
-                return DiskCacheStrategy.DATA;
-            case 3:
-                return DiskCacheStrategy.ALL;
-            case 4:
-                return DiskCacheStrategy.AUTOMATIC;
-            default:
-                return DiskCacheStrategy.NONE;
-        }
-    }
-
     private static Object getUrl(String url) {
         if (url.startsWith("data:")) return url;
-        url = DefaultConfig.checkReplaceProxy(url);
-
         String header = null;
         String referer = null;
         String ua = null;
         String cookie = null;
-
-        if (url.contains("doubanio.com") && !url.contains("@Referer=") && !url.contains("@User-Agent=")) {
-            url += "@Referer=https://api.douban.com/@User-Agent=" + UA.randomOne();
-        }
-
-        //检查链接里面是否有自定义header
         if (url.contains("@Headers=")) {
             header = url.split("@Headers=")[1].split("@")[0];
             try {
                 header = URLDecoder.decode(header, "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
+            } catch (UnsupportedEncodingException ignored) {
             }
         }
         if (url.contains("@Cookie=")) cookie = url.split("@Cookie=")[1].split("@")[0];
@@ -162,58 +102,40 @@ public class ImgUtil {
         url = url.split("@")[0];
         if (TextUtils.isEmpty(url)) return null;
 
-        /*   AuthInfo authInfo = new AuthInfo(url);
-        url = authInfo.url; */
-
         LazyHeaders.Builder builder = new LazyHeaders.Builder();
-
-        /*     if (!authInfo.auth.isEmpty()) {
-            builder.addHeader(HttpHeaders.AUTHORIZATION, authInfo.auth);
-        }*/
-
+        Map<String, String> headers = new HashMap<>();
         if (!TextUtils.isEmpty(header)) {
-            JsonObject jsonInfo = new Gson()
-                .fromJson(header, JsonObject.class);
-            for (String key: jsonInfo.keySet()) {
-                String val = jsonInfo.get(key)
-                    .getAsString();
-                builder.addHeader(key, val);
+            try {
+                JsonObject jsonInfo = new Gson().fromJson(header, JsonObject.class);
+                for (String key : jsonInfo.keySet()) {
+                    putHeader(headers, key, jsonInfo.get(key).getAsString());
+                }
+            } catch (Throwable ignored) {
             }
-        } else {
-            if (!TextUtils.isEmpty(cookie)) {
-                builder.addHeader(HttpHeaders.COOKIE, cookie);
-            }
-            if (!TextUtils.isEmpty(ua)) {
-                builder.addHeader(HttpHeaders.USER_AGENT, ua);
-            } else {
-                String mobile_UA = "Dalvik/2.1.0 (Linux; U; Android 13; M2102J2SC Build/TKQ1.220829.002)";
-                builder.addHeader(HttpHeaders.USER_AGENT, mobile_UA);
-            }
-            if (!TextUtils.isEmpty(referer)) builder.addHeader(HttpHeaders.REFERER, referer);
         }
-
-        try {
-            URL imgUrl = new URL(url);
-            String host = imgUrl.getHost();
-            builder.addHeader(HttpHeaders.HOST, host);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
+        putHeader(headers, "Cookie", cookie);
+        if (!TextUtils.isEmpty(ua)) putHeader(headers, "User-Agent", ua);
+        if (!TextUtils.isEmpty(referer)) putHeader(headers, "Referer", referer);
+        for (Map.Entry<String, String> entry : headers.entrySet()) builder.setHeader(entry.getKey(), entry.getValue());
         return new GlideUrl(url, builder.build());
+    }
+
+    private static void putHeader(Map<String, String> headers, String key, String value) {
+        if (TextUtils.isEmpty(key) || TextUtils.isEmpty(value)) return;
+        headers.put(key, value.trim());
     }
 
     private static RequestListener < Bitmap > getListener(ImageView view, ImageView.ScaleType scaleType) {
         return new RequestListener < Bitmap > () {
             @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target < Bitmap > target, boolean isFirstResource) {
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target <Bitmap> target, boolean isFirstResource) {
                 view.setScaleType(scaleType);
                 view.setImageResource(R.drawable.img_loading_placeholder);
                 return true;
             }
 
             @Override
-            public boolean onResourceReady(Bitmap resource, Object model, Target < Bitmap > target, DataSource dataSource, boolean isFirstResource) {
+            public boolean onResourceReady(Bitmap resource, Object model, Target <Bitmap> target, DataSource dataSource, boolean isFirstResource) {
                 view.setScaleType(scaleType);
                 return false;
             }
