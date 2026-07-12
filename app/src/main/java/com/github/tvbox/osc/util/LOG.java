323071f0ec4c702726bd2d3d6853536d646fb4dc
daji888 <97334834+daji888@users.noreply.github.com>
2026-07-12 15:51:06 +0800

    Update LOG.java

--- app/src/main/java/com/github/tvbox/osc/util/LOG.java
+++ app/src/main/java/com/github/tvbox/osc/util/LOG.java
@@ -12,7 +12,8 @@ import org.greenrobot.eventbus.EventBus;
  * @description:
  */
 public class LOG {
-    private static String TAG = "TVBox";
+    private static String TAG = "TVBox-runtime";
+    private static final int MAX_LOG_LENGTH = 3000;
 
     public static void e(Throwable t) {
         Log.e(TAG, t.getMessage(), t);
@@ -42,5 +43,29 @@ public class LOG {
     public static void i(String tag, String msg) {
         Log.i(tag, msg);
         EventBus.getDefault().post(new LogEvent(String.format("【I/%s】=>>>", tag) + msg));
+    }
+
+    public static void longI(String prefix, String msg) {
+        longLog(Log.INFO, prefix, msg);
+    }
+
+    public static void longE(String prefix, String msg) {
+        longLog(Log.ERROR, prefix, msg);
+    }
+
+    private static void longLog(int priority, String prefix, String msg) {
+        String text = msg == null ? "null" : msg;
+        String title = prefix == null ? "" : prefix;
+        int length = text.length();
+        if (length <= MAX_LOG_LENGTH) {
+            Log.println(priority, TAG, title + text);
+            return;
+        }
+        int count = (length + MAX_LOG_LENGTH - 1) / MAX_LOG_LENGTH;
+        for (int i = 0; i < count; i++) {
+            int start = i * MAX_LOG_LENGTH;
+            int end = Math.min(start + MAX_LOG_LENGTH, length);
+            Log.println(priority, TAG, title + "[" + (i + 1) + "/" + count + "] " + text.substring(start, end));
+        }
     }
 }
