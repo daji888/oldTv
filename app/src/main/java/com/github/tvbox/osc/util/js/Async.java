4ad5857e05efc4229d53f7abcc2a912a539cc98d
daji888 <97334834+daji888@users.noreply.github.com>
2026-07-16 11:40:55 +0800

    Update Async.java

--- app/src/main/java/com/github/tvbox/osc/util/js/Async.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/Async.java
@@ -1,58 +1,101 @@
 package com.github.tvbox.osc.util.js;
 
-import com.google.common.util.concurrent.SettableFuture;
 import com.whl.quickjs.wrapper.JSCallFunction;
 import com.whl.quickjs.wrapper.JSFunction;
 import com.whl.quickjs.wrapper.JSObject;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Async {
 
-    private final SettableFuture<Object> future;
+    private final Result future;
 
-    public static SettableFuture<Object> run(JSObject object, String name, Object[] args) {
+    private final JSCallFunction success = new JSCallFunction() {
+        @Override
+        public Object call(Object... args) {
+            future.complete(args != null && args.length > 0 ? args[0] : null);
+            return null;
+        }
+    };
+
+    private final JSCallFunction error = new JSCallFunction() {
+        @Override
+        public Object call(Object... args) {
+            String msg = args != null && args.length > 0 && args[0] != null ? args[0].toString() : "";
+            future.completeExceptionally(new Exception(msg));
+            return null;
+        }
+    };
+
+    public static Result run(JSObject object, String name, Object[] args) {
         return new Async().call(object, name, args);
     }
 
     private Async() {
-        this.future = SettableFuture.create();
+        this.future = new Result();
     }
 
-    private SettableFuture<Object> call(JSObject object, String name, Object[] args) {
+    private Result call(JSObject object, String name, Object[] args) {
+        JSFunction function = object.getJSFunction(name);
+        if (function == null) return empty();
         try {
-            JSFunction function = object.getJSFunction(name);
-            if (function == null) {
-                future.set(null);
-                return future;
-            }
             Object result = function.call(args);
-            if (result instanceof JSObject) {
-                then(result);
-            } else {
-                future.set(result);
-            }
+            if (result instanceof JSObject) then((JSObject) result);
+            else future.complete(result);
         } catch (Throwable t) {
-            future.setException(t);
+            future.completeExceptionally(t);
+        } finally {
+            function.release();
         }
         return future;
     }
 
-    private void then(Object result) {
-        JSObject promise = (JSObject) result;
-        JSFunction thenFn = promise.getJSFunction("then");
-        if (thenFn != null) {
-            thenFn.call(callback);
+    private Result empty() {
+        future.complete(null);
+        return future;
+    }
+
+    private void then(JSObject promise) {
+        JSFunction then = promise.getJSFunction("then");
+        if (then == null) {
+            future.complete(promise);
         } else {
-            // If there's no then, complete immediately
-            future.set(result);
+            consume(then, success);
+            consume(promise.getJSFunction("catch"), error);
         }
     }
 
-    private final JSCallFunction callback = new JSCallFunction() {
-        @Override
-        public Object call(Object... args) {
-            // args[0] holds the resolved value from the JS promise
-            future.set(args.length > 0 ? args[0] : null);
-            return null;
+    private void consume(JSFunction function, JSCallFunction callback) {
+        if (function == null) return;
+        try {
+            function.call(callback);
+        } finally {
+            function.release();
         }
-    };
+    }
+
+    public static class Result {
+        private final CountDownLatch latch = new CountDownLatch(1);
+        private Object value;
+        private Throwable error;
+
+        void complete(Object value) {
+            this.value = value;
+            latch.countDown();
+        }
+
+        void completeExceptionally(Throwable error) {
+            this.error = error;
+            latch.countDown();
+        }
+
+        public Object get() throws Exception {
+            latch.await();
+            if (error != null) {
+                if (error instanceof Exception) throw (Exception) error;
+                throw new Exception(error);
+            }
+            return value;
+        }
+    }
 }
