19687eababed19d926455918b95c59f5a76d1132
daji888 <97334834+daji888@users.noreply.github.com>
2026-07-31 15:08:31 +0800

    Update Crypto.java

--- app/src/main/java/com/github/tvbox/osc/util/js/Crypto.java
+++ app/src/main/java/com/github/tvbox/osc/util/js/Crypto.java
@@ -2,11 +2,9 @@ package com.github.tvbox.osc.util.js;
 
 import android.util.Base64;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
@@ -27,33 +25,25 @@ public class Crypto {
             SecretKeySpec keySpec = new SecretKeySpec(keyBuf, "AES");
             if (iv == null) cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, keySpec);
             else cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(ivBuf));
-            byte[] inBuf = inBase64 ? Base64.decode(input.replaceAll("_", "/").replaceAll("-", "+"), Base64.DEFAULT) : input.getBytes("UTF-8");
-            return outBase64 ? Base64.encodeToString(cipher.doFinal(inBuf), Base64.NO_WRAP) : new String(cipher.doFinal(inBuf), "UTF-8");
+            byte[] inBuf = inBase64 ? Base64.decode(input.replaceAll("_", "/").replaceAll("-", "+"), Base64.DEFAULT) : input.getBytes(StandardCharsets.UTF_8);
+            return outBase64 ? Base64.encodeToString(cipher.doFinal(inBuf), Base64.NO_WRAP) : new String(cipher.doFinal(inBuf), StandardCharsets.UTF_8);
         } catch (Exception e) {
             e.printStackTrace();
             return "";
         }
     }
 
-    public static String rsa(boolean pub, boolean encrypt, String input, boolean inBase64, String key, boolean outBase64) {
+    public static String rsa(String mode, boolean pub, boolean encrypt, String input, boolean inBase64, String key, boolean outBase64) {
         try {
             Key rsaKey = generateKey(pub, key);
-            int len = getModulusLength(rsaKey);
-            byte[] outBytes = new byte[0];
-            byte[] inBytes = inBase64 ? Base64.decode(input.replaceAll("_", "/").replaceAll("-", "+"), Base64.DEFAULT) : input.getBytes("UTF-8");
-            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            byte[] inBytes = inBase64 ? Base64.decode(input.replaceAll("_", "/").replaceAll("-", "+"), Base64.DEFAULT) : input.getBytes(StandardCharsets.UTF_8);
+            String tranformation = "RSA/ECB/PKCS1Padding";
+            if ("RSA/PKCS1".equals(mode)) tranformation = "RSA/ECB/PKCS1Padding";
+            else if ("RSA/None/NoPadding".equals(mode)) tranformation = "RSA/None/NoPadding";
+            Cipher cipher = Cipher.getInstance(tranformation);
             cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, rsaKey);
-            int blockLen = encrypt ? len / 8 - 11 : len / 8;
-            int bufIdx = 0;
-            while (bufIdx < inBytes.length) {
-                int bufEndIdx = Math.min(bufIdx + blockLen, inBytes.length);
-                byte[] tmpInBytes = new byte[bufEndIdx - bufIdx];
-                System.arraycopy(inBytes, bufIdx, tmpInBytes, 0, tmpInBytes.length);
-                byte[] tmpBytes = cipher.doFinal(tmpInBytes);
-                bufIdx = bufEndIdx;
-                outBytes = concatArrays(outBytes, tmpBytes);
-            }
-            return outBase64 ? Base64.encodeToString(outBytes, Base64.NO_WRAP) : new String(outBytes, "UTF-8");
+            byte[] outBytes = cipher.doFinal(inBytes);
+            return outBase64 ? Base64.encodeToString(outBytes, Base64.NO_WRAP) : new String(outBytes, StandardCharsets.UTF_8);
         } catch (Exception e) {
             e.printStackTrace();
             return "";
@@ -61,22 +51,9 @@ public class Crypto {
     }
 
     private static Key generateKey(boolean pub, String key) throws Exception {
-        if (pub) key = key.replaceAll("\r\n", "").replaceAll("\n", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "");
-        else key = key.replaceAll("\r\n", "").replaceAll("\n", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
+        if (pub) key = key.replaceAll("[\\r\\n]", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "");
+        else key = key.replaceAll("[\\r\\n]", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
         return pub ? KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.decode(key, Base64.DEFAULT))) : KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(Base64.decode(key, Base64.DEFAULT)));
     }
 
-    private static int getModulusLength(Key key) {
-        if (key instanceof PublicKey) return ((RSAPublicKey) key).getModulus().bitLength();
-        else return ((RSAPrivateKey) key).getModulus().bitLength();
-    }
-
-    private static byte[] concatArrays(byte[] a, byte[] b) {
-        int aLen = a.length;
-        int bLen = b.length;
-        byte[] result = new byte[aLen + bLen];
-        System.arraycopy(a, 0, result, 0, aLen);
-        System.arraycopy(b, 0, result, aLen, bLen);
-        return result;
-    }
 }
