09c73b1d65657d82bee938ab277255affa56a4e8
daji888 <97334834+daji888@users.noreply.github.com>
2026-08-14 17:51:52 +0800

    Create RequestInterceptor.java

---
+++ app/src/main/java/com/github/catvod/net/interceptor/RequestInterceptor.java
@@ -0,0 +1,41 @@
+package com.github.catvod.net.interceptor;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestInterceptor implements Interceptor {
+
+    private final ConcurrentHashMap<String, String> authMap;
+
+    public RequestInterceptor() {
+        authMap = new ConcurrentHashMap<>();
+    }
+
+    public void clear() {
+        authMap.clear();
+    }
+
+    @NonNull
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        Request.Builder builder = request.newBuilder();
+        HttpUrl url = request.url();
+        checkAuth(url, builder);
+        return chain.proceed(builder.build());
+    }
+
+    private void checkAuth(HttpUrl url, Request.Builder builder) {
+        String host = url.host();
+        String auth = url.queryParameter("auth");
+        if (auth != null) authMap.put(host, auth);
+        else if (authMap.containsKey(host)) builder.url(url.newBuilder().addQueryParameter("auth", authMap.get(host)).build());
+    }
+}
