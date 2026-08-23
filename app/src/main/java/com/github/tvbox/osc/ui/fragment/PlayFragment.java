b98eec7e66d9b0653aa5f96cf5ef8075f5ac16b2
daji888 <97334834+daji888@users.noreply.github.com>
2026-08-23 17:13:18 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -1896,7 +1896,7 @@ public class PlayFragment extends BaseLazyFragment {
              super.onPageFinished(view, url);
              LOG.i("echo-onPageFinished url:" + url);
              if (!url.equals("about:blank")) {
-                mController.evaluateScript(sourceBean, url, view, null);
+                mController.evaluateScript(sourceBean, url, view);
             }
             mHandler.sendEmptyMessage(200);
         }
