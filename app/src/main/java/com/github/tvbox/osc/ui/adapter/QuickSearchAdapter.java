24d5d92b4d933d3d6c2c3275cff9cbf0cdef86c5
daji888 <97334834+daji888@users.noreply.github.com>
2026-08-28 15:17:05 +0800

    Update QuickSearchAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/QuickSearchAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/QuickSearchAdapter.java
@@ -5,7 +5,6 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.api.ApiConfig;
 import com.github.tvbox.osc.bean.Movie;
-// import com.github.tvbox.osc.util.ImgUtil;
 import java.util.ArrayList;
 
 /**
@@ -22,20 +21,5 @@ public class QuickSearchAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHo
     protected void convert(BaseViewHolder helper, Movie.Video item) {
         // lite
         helper.setText(R.id.tvName, String.format("%s  %s %s %s", ApiConfig.get().getSource(item.sourceKey).getName(), item.name, item.type == null ? "" : item.type, item.note == null ? "" : item.note));
-        // with preview
-        /*
-        helper.setText(R.id.tvName, item.name);
-        helper.setText(R.id.tvSite, ApiConfig.get().getSource(item.sourceKey).getName());
-        helper.setVisible(R.id.tvNote, item.note != null && !item.note.isEmpty());
-        if (item.note != null && !item.note.isEmpty()) {
-            helper.setText(R.id.tvNote, item.note);
-        }
-        ImageView ivThumb = helper.getView(R.id.ivThumb);
-        if (!TextUtils.isEmpty(item.pic)) {
-            ImgUtil.load(item.pic, ivThumb, 10);
-        } else {
-            ivThumb.setImageResource(R.drawable.error_loading);
-        }
-        */
     }
 }
