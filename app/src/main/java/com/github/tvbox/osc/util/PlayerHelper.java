01bb732cea730d3efc76b99b9438792d428c0e25
daji888 <97334834+daji888@users.noreply.github.com>
2026-09-06 16:50:53 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -185,10 +185,6 @@ public class PlayerHelper {
         return existPlayers;
     }
 
-    public static Boolean runExternalPlayer(int playerType, Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
-        return runExternalPlayer(playerType, activity, url, title, subtitle, headers);
-    }
-
     public static Boolean runExternalPlayer(int playerType, Activity activity, String url, String title, String subtitle, HashMap<String, String> headers, long progress) {
         boolean callResult = false;
         switch (playerType) {
